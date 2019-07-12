df85d110d748671e46d6b76c04184be2713bd949
john <john@cribl.io>
2019-07-12 17:47:28 -0500

    (CRIBL-1040) Initial implementation and test of log.c.

--- src/log.c
+++ src/log.c
@@ -1,42 +1,65 @@
 #include <stddef.h>
+#include <stdlib.h>
 #include "log.h"
 
 struct _log_t
 {
-
+    transport_t* transport;
+    cfg_log_level_t level;
 };
+
+#define DEFAULT_LEVEL CFG_LOG_ERROR
 
 log_t*
 logCreate()
 {
-    return NULL;
+    log_t* log = calloc(1, sizeof(log_t));
+    if (!log) return NULL;
+
+    log->level = DEFAULT_LEVEL;
+
+    return log;
 }
 
 void
 logDestroy(log_t** log)
 {
+    if (!log || !*log) return;
+    log_t* l = *log;
+    transportDestroy(&l->transport);
+    free(l);
+    *log = NULL;
 }
 
 int
 logSend(log_t* log, char* msg)
 {
-    return -1;
+    if (!log || !msg) return -1;
+
+    return transportSend(log->transport, msg);
 }
 
 cfg_log_level_t
 logLevel(log_t* log)
 {
-    return CFG_LOG_NONE;
+    return (log) ? log->level : DEFAULT_LEVEL;
 }
 
 void
 logSetTransport(log_t* log, transport_t* transport)
 {
+    if (!log) return;
+
+    // Don't leak if logSetTransport is called repeatedly
+    transportDestroy(&log->transport);
+    log->transport = transport;
 }
 
 void
 logLevelSet(log_t* log, cfg_log_level_t level)
 {
+    if (!log) return;
+    log->level = level;
 }
 
 

5b26790246b40db4df43b9ea7750c35612a3ee8e
john <john@cribl.io>
2019-09-23 21:46:42 -0500

    Merge coverage without gcov-tool.  It's not available on mac.

--- test/execute.sh
+++ test/execute.sh
@@ -1,50 +1,35 @@
 #!/bin/bash
 
+NUM=0
+INFOLIST=""
+ENVVARS=""
+
 accumulate_coverage() {
     # This function accumulates coverage info (gcda files) from test
     # to test.  It merges new coverage info (current dir) into coverage
     # info from previous tests (coverage.tmp dir).
 
+    FILE=coverage/coverage$NUM.info
+    (unset DYLD_LIBRARY_PATH; lcov --capture --directory . --output-file $FILE)
+    INFOLIST=$INFOLIST"$FILE "
+    rm *\.gcda
+    ((NUM++))
 
-    # the first time this runs, coverage.tmp won't exist.  Create it.
-    if [ ! -d coverage.tmp ]; then
-        gcov-tool rewrite . -o coverage.tmp
-        rm *gcda
-        return 0
-    fi
-
-    # on subsequent iterations, merge with coverage.tmp
-    gcov-tool merge . coverage.tmp -o coverage.merged
-    rm *gcda
-
-    # move coverage.merged to coverage.tmp
-    rm -rf coverage.tmp
-    gcov-tool rewrite coverage.merged -o coverage.tmp
-    rm -rf coverage.merged
     return 0
 }
 
-return_coverage() {
-    # This function moves accumlated coverage info
-    # (from coverage.tmp dir) into the current dir
-    gcov-tool rewrite coverage.tmp -o .
-    rm -rf coverage.tmp
+report_final_coverage() {
 
+    genhtml -o coverage $INFOLIST
+    rm *\.gcno
 }
 
 run_test() {
 
-
-    # print out instructions for how to run one at a time...
-    if [ ! -z "${LD_LIBRARY_PATH}" ]; then
-        echo "Running LD_LIBRARY_PATH=$LD_LIBRARY_PATH $1"
-    fi
-    if [ ! -z "${DYLD_LIBRARY_PATH}" ]; then
-        echo "Running DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH $1"
-    fi
+    echo "$ENVVARS$1"
 
     # run the test
-    $1
+    (export $ENVVARS; $1)
 
     # accumulate errors reported by the return value of the test
     ERR+=$?
@@ -57,10 +42,10 @@ CWD="$(pwd)"
 
 if uname -s 2> /dev/null | grep -i "linux" > /dev/null; then
     OS="linux"
-    export LD_LIBRARY_PATH=contrib/cmocka/build/src/
+    ENVVARS=$ENVVARS"LD_LIBRARY_PATH=contrib/cmocka/build/src/ "
 elif uname -s 2> /dev/null | grep -i darwin > /dev/null; then
     OS="macOS"
-    export DYLD_LIBRARY_PATH=contrib/cmocka/build/src/
+    ENVVARS=$ENVVARS"DYLD_LIBRARY_PATH=contrib/cmocka/build/src/ "
 else
     OS="unknown"
 fi
@@ -93,28 +78,18 @@ fi
 
 
 # wraptest has special requirements, env wise...
-export SCOPE_HOME=${CWD}/test/
+ENVVARS="SCOPE_HOME=${CWD}/test/ "
 if [ "${OS}" = "linux" ]; then
-    export LD_LIBRARY_PATH=lib/${OS}:${LD_LIBRARY_PATH}
+    ENVVARS=$ENVVARS"LD_LIBRARY_PATH=lib/${OS}:contrib/cmocka/build/src/ "
 elif [ "${OS}" = "macOS" ]; then
-    export DYLD_LIBRARY_PATH=lib/${OS}:${DYLD_LIBRARY_PATH}
-    export DYLD_INSERT_LIBRARIES=${CWD}/lib/${OS}/libwrap.so
-    export DYLD_FORCE_FLAT_NAMESPACE=1
+    ENVVARS=$ENVVARS"DYLD_LIBRARY_PATH=lib/${OS}:contrib/cmocka/build/src/ "
+    ENVVARS=$ENVVARS"DYLD_INSERT_LIBRARIES=${CWD}/lib/${OS}/libwrap.so "
+    ENVVARS=$ENVVARS"DYLD_FORCE_FLAT_NAMESPACE=1 "
 fi
 run_test test/${OS}/wraptest
 
 
-return_coverage
+report_final_coverage
 
-
-# I think this is unnecessary, but...
-unset SCOPE_HOME
-if [ "${OS}" = "linux" ]; then
-    unset LD_LIBRARY_PATH
-elif [ "${OS}" = "macOS" ]; then
-    unset DYLD_LIBRARY_PATH
-    unset DYLD_INSERT_LIBRARIES
-    unset DYLD_FORCE_FLAT_NAMESPACE
-fi
 
 exit ${ERR}

74375f38a10289d619abacbe45615eebd8c6184e
Artiom Voitenkov <avoitenkov@objectstyle.com>
2019-10-03 16:21:06 +0300

    added wrapped tests

--- test/ltp_test_runner/runner.py
+++ test/ltp_test_runner/runner.py
@@ -15,14 +15,14 @@ help_text = '''options:
 
 
 class Runner:
-    def __init__(self, home, include_tests, exclude_tests):
+    def __init__(self, home, include_tests, exclude_tests, lib_path):
         self.__home = home
         self.__include_tests = include_tests
         self.__exclude_tests = exclude_tests
         self.__test_watcher = w.Watcher()
+        self.__lib_path = lib_path
 
     def run(self):
-
         self.__test_watcher.start()
 
         for test in self.__include_tests:
@@ -30,39 +30,45 @@ class Runner:
                 if os.path.isdir(test):
                     for d in os.listdir(test):
                         if d not in self.__exclude_tests:
-                            self.__execute_test(self.__home + d, self.__test_watcher)
+                            self.__find_tests(self.__home + d)
                 else:
-                    self.__execute_test(self.__home + test, self.__test_watcher)
+                    self.__find_tests(self.__home + test)
 
         self.__test_watcher.finish()
         return (0, 1)[self.__test_watcher.has_failures()]
 
-    def __execute_test(self, test_path, test_watcher):
+    def __find_tests(self, test_path):
         if not os.path.isdir(test_path):
-            return 0
+            return
 
         for f in os.listdir(test_path):
             filename = test_path + '/' + f
             if os.path.isfile(filename) and os.access(filename, os.X_OK):
-                start = time.time()
-                proc = subprocess.Popen('./' + f, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=test_path)
-                stdout, stderr = proc.communicate()
-                end = time.time()
-                rc = proc.returncode
+                self.__execute_test(test_path, f, False)
+                self.__execute_test(test_path, f, True)
 
-                if rc == 0:
-                    test_watcher.test_passed(w.TestResult(f, False, stdout, stderr, end - start, rc))
-                else:
-                    test_watcher.test_failed(w.TestResult(f, False, stdout, stderr, end - start, rc))
+    def __execute_test(self, test_path, test, wrapped):
+        start = time.time()
+        env = dict(os.environ)
 
+        if wrapped:
+            env['LD_PRELOAD'] = self.__lib_path
 
-        return 0
+        p = subprocess.Popen('./' + test, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=test_path, env=env)
+        stdout, stderr = p.communicate()
+
+        self.__send_result(w.TestResult(test, wrapped, stdout, stderr, time.time() - start, p.returncode))
+
+    def __send_result(self, result):
+        return (self.__test_watcher.test_failed(result), self.__test_watcher.test_passed(result))[
+            result.return_code == 0]
 
 
 def main(args):
     home = "/opt/test/ltp/testcases/kernel/syscalls/"
     include_tests = []
     exclude_tests = []
+    lib_path = "/opt/scope/lib/linux/libwrap.so"
 
     # parse args
     for i in range(len(args)):
@@ -78,6 +84,9 @@ def main(args):
         if args[i] == 'exclude_tests':
             exclude_tests = args[i + 1]
 
+        if args[i] == 'lib_path':
+            lib_path = args[i + 1]
+
         if args[i] == '--config':
             path = args[i + 1]
             with open(path, "rb") as f:
@@ -85,6 +94,7 @@ def main(args):
                 home = config["home"]
                 include_tests = config["include_tests"]
                 exclude_tests = config["exclude_tests"]
+                lib_path = config["lib_path"]
 
     if not home.endswith('/'):
         home += '/'
@@ -93,7 +103,9 @@ def main(args):
         sys.stderr.write("tests home is not valid")
         return 1
 
-    return Runner(home, include_tests, exclude_tests).run()
+    subprocess.call("make", stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd="/opt/scope")
+
+    return Runner(home, include_tests, exclude_tests, lib_path).run()
 
 
 if __name__ == "__main__":

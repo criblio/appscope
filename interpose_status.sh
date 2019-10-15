873df24ceb57dd0d573f73e2cb36a68ef2fc87ba
john <john@cribl.io>
2019-10-15 21:54:58 +0000

    (CRIBL-1295).  Get ./interpose_status.sh to work on mac and linux at same time.

--- interpose_status.sh
+++ interpose_status.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
 LIB_NAME=libwrap.so
+
+
+# Setup some things based on platform
+if uname -s 2> /dev/null | grep -i "linux" > /dev/null; then
+    PRELOAD=LD_PRELOAD
+    REL_PATH=lib/linux/${LIB_NAME}
+    NM_OPTS=-D
+elif uname -s 2> /dev/null | grep -i "darwin" > /dev/null; then
+    PRELOAD=DYLD_INSERT_LIBRARIES
+    REL_PATH=lib/macOS/${LIB_NAME}
+    NM_OPTS=-a
+else
+    echo "ERROR: Undetermined platform.  Exiting..." >&2
+    exit 1
+fi
+
+
 # We need to resolve the scope library to see what it interposes.
 # In priority order, use:
 #  1) LD_PRELOAD / DYLD_INSERT_LIBRARIES env variables
@@ -11,10 +28,8 @@ if [[ ${LD_PRELOAD} == *"$LIB_NAME"* ]]; then
 elif [[ ${DYLD_INSERT_LIBRARIES} == *"$LIB_NAME"* ]]; then
     LIB_PATH=${DYLD_INSERT_LIBRARIES}
 elif [ ! -z "$CRIBL_HOME" ]; then
-    if [ -f "${CRIBL_HOME}/lib/linux/$LIB_NAME" ]; then
-        LIB_PATH="${CRIBL_HOME}/lib/linux/$LIB_NAME"
-    elif [ -f "$CRIBL_HOME/lib/macOS/$LIB_NAME" ]; then
-        LIB_PATH="$CRIBL_HOME/lib/macOS/$LIB_NAME"
+    if [ -f "${CRIBL_HOME}/${REL_PATH}" ]; then
+        LIB_PATH="${CRIBL_HOME}/${REL_PATH}"
     fi
 else
     LIB_FIND=`find . -type f -name "$LIB_NAME" | grep -v dSYM | head -n1`
@@ -24,18 +39,18 @@ else
 fi
 
 if [ -z $LIB_PATH ]; then
+    echo "ERROR." >&2
     echo "Couldn't find $LIB_NAME which is required for $0 to give helpful feedback." >&2
     echo "Please rerun with one of the following changes:" >&2
-    echo " o) set LD_PRELOAD env variable with path to $LIB_NAME (assuming linux)" >&2
-    echo " o) set DYLD_INSERT_LIBRARIES env variable with path to $LIB_NAME (assuming mac)" >&2
-    echo " o) set CRIBL_HOME env variable to a directory which could resolve " >&2
-    echo "        lib/linux/$LIB_NAME (linux) or lib/macOS/$LIB_NAME (mac)" >&2
+    echo " o) set ${PRELOAD} env variable with path to $LIB_NAME" >&2
+    echo " o) set CRIBL_HOME env variable to a directory which could resolve ${REL_PATH}" >&2
     echo " o) run this script from a directory which contains or is a parent of $LIB_NAME" >&2
     exit 1
 fi
 
+# We need to get a command as an argument
 if [ -z "$1" ]; then
-    echo "$0 requires a command as an argument." >&2
+    echo "ERROR: $0 requires a command as an argument." >&2
     echo "  e.g. $0 /bin/ps" >&2
     exit 1
 fi
@@ -47,17 +62,19 @@ else
 fi
 
 if [ -z "$CMD" ]; then
-    echo "Could not resolve $1 as a command.  Try specifying an absolute path." >&2
+    echo "ERROR: Could not resolve $1 as a command.  Try specifying an absolute path." >&2
     exit 1
 fi
 
-echo "Processing ${CMD}"
+
+# We should be good to go!
+echo "Processing ${CMD} with scope library ${LIB_PATH}"
 
 
-INTERPOSED_FNS=`nm -a ${LIB_PATH} | grep " T " | cut -c20-100 | grep -vE "(_init)|(_fini)"`
+INTERPOSED_FNS=`nm ${NM_OPTS} ${LIB_PATH} | grep " T " | cut -c20-100 | grep -vE "(_init)|(_fini)"`
 #echo "${INTERPOSED_FNS}"
 
-nm -a $CMD | grep -E " [WU] " | cut -c20-100 >> ./nm.out
+nm ${NM_OPTS} $CMD | grep -E " [WU] " | cut -c20-100 > ./nm.out
 NM_FUNCTIONS_NUM=`cat ./nm.out | wc -l`
 
 SCOPE_NUM=0

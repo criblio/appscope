7d60a654279c37010721bd8e5d591090740ede64
evintik <evintik@objectstyle.com>
2019-10-17 16:22:32 +0200

    Storing results to file

--- test/testContainers/applications_test_runner/reporting.py
+++ test/testContainers/applications_test_runner/reporting.py
@@ -1,12 +1,55 @@
+import os
 from typing import Dict
-from common import TestSetResult
+from common import TestSetResult, TestExecutionData
 from tabulate import tabulate
+import json
+
+from watcher import TestWatcher
 
 
 def get_status(result):
     if not result:
         return "-"
     return "fail" if not result.passed else "pass"
+
+
+def store_results_to_file(watcher: TestWatcher, path: str):
+
+    def exec_data_to_json(exec_data: TestExecutionData):
+        return {
+            "status": get_status(exec_data.result),
+            "scope_messages": exec_data.scope_messages,
+            "error": exec_data.result.error if exec_data.result else None,
+            "duration": exec_data.duration,
+            "test_data": exec_data.test_data
+        }
+
+    with open(os.path.join(path, f"test_results_{watcher.execution_id}.json"), "a") as f:
+        results = watcher.get_all_results()
+        for test, result in results.items():
+            log_row = {
+                "id": watcher.execution_id,
+                "type": "test_results",
+                "test": test,
+                "status": get_status(result),
+                "scoped_exec_data": exec_data_to_json(result.scoped_execution_data),
+                "unscoped_exec_data": exec_data_to_json(result.unscoped_execution_data),
+                "error": result.error
+            }
+            json.dump(log_row, f, default=lambda o: o.__dict__)
+
+        summary_row = {
+            "id": watcher.execution_id,
+            "type": "summary",
+            "execution_error": watcher.execution_error,
+            "status": "pass" if not watcher.has_failures() else "fail",
+            "start_date": watcher.start_date.isoformat(),
+            "finish_date": watcher.finish_date.isoformat(),
+            "total_tests": len(results),
+            "failed_tests": len([res for res in results.values() if not res.passed]),
+        }
+
+        json.dump(summary_row, f)
 
 
 def print_summary(results: Dict[str, TestSetResult]):
@@ -30,5 +73,5 @@ def print_summary(results: Dict[str, TestSetResult]):
     print("Total failed:\t" + str(total_failed))
     table = [to_table_row(k, v) for k, v in results.items()]
 
-    print(tabulate(table, headers=["Test", "Result", "Result unscoped/scoped", "Duration unscoped/scoped","Messages uscoped/scoped","Error"]))
-
+    print(tabulate(table, headers=["Test", "Result", "Result unscoped/scoped", "Duration unscoped/scoped",
+                                   "Messages uscoped/scoped", "Error"]))

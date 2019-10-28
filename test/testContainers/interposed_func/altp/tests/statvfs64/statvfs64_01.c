7814d2da1559a4e8d921eacfb87e0524c10e690e
Artiom Voitenkov <avoitenkov@objectstyle.com>
2019-10-28 18:53:15 +0300

    added new tests

---
+++ test/testContainers/interposed_func/altp/tests/statvfs64/statvfs64_01.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/statvfs.h>
+
+#include "test_utils.h"
+
+#define TEST_MSG "test"
+
+int do_test() {
+    int test_result = EXIT_SUCCESS;   
+    struct statvfs fs;
+
+    if(statvfs64("/", &fs) < 0) {
+        TEST_ERROR();
+    }
+
+    FILE* fp = popen("stat / -c \"%s\"", "r");
+
+    if (fp == NULL)  {
+        TEST_ERROR();
+    } else {
+        char buf[BUFSIZ];
+        int f_bsize = 0;
+        size_t byte_count = fread(buf, 1, BUFSIZ - 1, fp);
+        buf[byte_count] = 0;
+        pclose(fp);
+
+        sscanf(buf, "%d", &f_bsize);
+
+        if(fs.f_bsize != f_bsize) {
+            TEST_ERROR();
+        }
+    }
+
+    return test_result;
+}

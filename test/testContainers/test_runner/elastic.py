a58e41b6cdb4f122c1d11404da52d4ed37949524
evintik <evintik@objectstyle.com>
2019-11-08 12:47:54 +0300

    CRIBL-1529 - search test

--- test/testContainers/test_runner/elastic.py
+++ test/testContainers/test_runner/elastic.py
@@ -1,4 +1,5 @@
 import logging
+from logging import DEBUG
 from typing import Tuple, Any
 
 from elasticsearch import Elasticsearch, helpers
@@ -23,23 +24,50 @@ class ElasticIndexAndSearchTest(ApplicationTest):
 
         logging.info(f"Elastic info: {es.info()}")
 
-        documents_num = 1000
+        documents_num = 100000
         logging.info(f"Sending {documents_num} documents to Elastic.")
 
         index = f"test_{random_string(4)}"
-        es.indices.create(index=index)
+        es.indices.create(
+            index=index,
+            body={
+                "mappings": {
+                    "properties": {
+                        "bucket": {"type": "integer"},
+                        "body": {"type": "keyword"},
+                    }
+                }
+            }
+        )
 
-        helpers.bulk(es, self.__documents_generator(documents_num), index=index)
+        helpers.bulk(es, self.documents_generator(documents_num), index=index)
 
         logging.info("Refreshing elastic index")
         es.indices.refresh(index=index)
 
         docs_in_index = es.count(index=index)["count"]
 
+        limit = 2
+        expected_results = int(documents_num / 10)
+        sr = es.search(
+            index=index,
+            body={
+                "query": {
+                    "term": {"bucket": 2}
+                },
+                "sort": [{"body": {"order": "desc"}}],
+                "size": limit,
+            }
+        )
+
         return validate_all(
             (docs_in_index == documents_num,
-             f"Expected to have {documents_num} docs in index, but found {docs_in_index}")
-        ), None
+             f"Expected to have {documents_num} docs in index, but found {docs_in_index}"),
+            (sr['hits']['total']['value'] == expected_results,
+             f"Expected to have {expected_results} search results, but found {sr['hits']['total']['value']}"),
+            (len(sr['hits']['hits']) == limit,
+             f"Expected to have {limit} hits returned, but found {len(sr['hits']['hits'])}"),
+        ), sr
 
     @retry(stop_max_attempt_number=5, wait_fixed=5000)
     def __connect_to_es(self):
@@ -48,14 +76,12 @@ class ElasticIndexAndSearchTest(ApplicationTest):
         assert elasticsearch.ping()
         return elasticsearch
 
-    def __documents_generator(self, documents_num):
+    @staticmethod
+    def documents_generator(documents_num):
         for i in range(documents_num):
             yield {
-                "_type": "document",
-                "doc": {
-                    "type": i % 10,
-                    "body": random_string(15)
-                }
+                "bucket": i % 10,
+                "body": random_string(15)
             }
 
     @property
@@ -68,3 +94,41 @@ def configure(runner: Runner, config):
                                              config.logs_path, start_wait=15)
 
     runner.add_tests([ElasticIndexAndSearchTest(app_controller)])
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=DEBUG)
+    es = Elasticsearch(hosts=[{"host": "localhost"}])
+
+    index = f"test_{random_string(4)}"
+
+    es.indices.create(
+        index=index,
+        body={
+            "mappings": {
+                "properties": {
+                    "bucket": {"type": "integer"},
+                    "body": {"type": "keyword"},
+                }
+            }
+        }
+    )
+
+    helpers.bulk(es, ElasticIndexAndSearchTest.documents_generator(100), index=index)
+
+    es.search(
+        index=index,
+        body={
+            "query": {
+                "term": {"user": "kimchy"}
+            },
+            "size": 2,
+        }
+    )
+
+    es.search(
+        index=index,
+        body={
+            "size": 8,
+        }
+    )

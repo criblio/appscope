fe7657a752a9c0bb98317c55470b572bd1856a55
john <john@cribl.io>
2019-12-10 21:57:26 +0000

    (CRIBL-1617) Fix build error on mac.

--- test/formattest.c
+++ test/formattest.c
@@ -1,11 +1,11 @@
 #include <errno.h>
+#include <float.h>
 #include <limits.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <values.h>
 #include "dbg.h"
 #include "format.h"
 

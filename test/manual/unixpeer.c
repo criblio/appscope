5dcc51c68b385251b81779998678c199391cd978
Donn <donn@cribl.io>
2020-03-16 14:16:33 -0500

    Added explicit sync to the unix socket peer test.

--- test/manual/unixpeer.c
+++ test/manual/unixpeer.c
@@ -1,7 +1,7 @@
 /* 
  * unixpeer.c 
  *
- * gcc -g -Wall test/manual/unixpeer.c -o test/linux/unixpeer
+ * gcc -g -Wall test/manual/unixpeer.c -lpthread -o test/linux/unixpeer
  *
  * Create a UNIX socket between 2 procs; client & server
  * Send data over the socket
@@ -26,8 +26,11 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <sys/wait.h>
+#include <semaphore.h>
+
 
 #define EVENTFILE "/tmp/scope_peer.log"
+#define SEMNAME "/sync"
 
 char *pfile;
 int verbose = 0;
@@ -101,9 +104,20 @@ server_child()
     unsigned int clientlen;
     int optval;
     int rc;
+    sem_t *sem;
     struct sockaddr_un serveraddr;
     struct sockaddr_un clientaddr;
     char buf[128];
+
+    if ((sem = sem_open(SEMNAME, O_RDWR)) == SEM_FAILED) {
+        perror("Server:sem_open");
+        exit(-1);
+    }
+
+    if (sem_init(sem, 1, 0) == -1) {
+        perror("Server:sem_init");
+        exit(-1);
+    }
 
     // socket: create the parent socket 
     parentfd = socket(AF_UNIX, SOCK_STREAM, 0);
@@ -145,9 +159,22 @@ server_child()
     // wait for a connection request then echo
     fprintf(stdout, "Parent accepting\n");
     clientlen = sizeof(clientaddr);
+
+    // Tell the client ready to connect
+    if (sem_post(sem) == -1) {
+        perror("Server:sem_post1");
+        exit(-1);
+    }
+
     childfd = accept(parentfd, (struct sockaddr *) &clientaddr, &clientlen);
     if (childfd < 0) {
         perror("ERROR on accept");
+        exit(-1);
+    }
+
+    // Tell the client we are ready to get data
+    if (sem_post(sem) == -1) {
+        perror("Server:sem_post2");
         exit(-1);
     }
 
@@ -166,19 +193,29 @@ server_child()
         // echo input to stdout
         if (verbose > 0) write(1, buf, rc);
                 
+        // Tell the client we got the data
+        if (sem_post(sem) == -1) {
+            perror("Server:sem_post3");
+            exit(-1);
+        }
+
     } while (1);
 
+    sem_close(sem);
     exit(0);
 }
 
 int
 client_child() {
     int clientsock;
+    sem_t *sem;
     struct sockaddr_un clientaddr;
     char test_data[] = "Passing the buck\n";
 
-    struct timespec ts = {.tv_sec=0, .tv_nsec=010000000}; // 10 ms
-    nanosleep(&ts, NULL);
+    if ((sem = sem_open(SEMNAME, O_RDWR)) == SEM_FAILED) {
+        perror("Client:sem_open");
+        exit(-1);
+    }
 
     if (verbose > 0) fprintf(stdout, "Child starting\n");
 
@@ -196,17 +233,23 @@ client_child() {
     // pfile is the socket path
     strncpy(clientaddr.sun_path, pfile, sizeof(clientaddr.sun_path)-1);
 
+    // Is server ready to connect
+    if (sem_wait(sem) == -1) {
+        perror("Client:sem_wait1");
+        exit(-1);
+    }
+
     if (connect(clientsock, (const struct sockaddr *)&clientaddr,
                 sizeof(struct sockaddr_un)) == -1) {
         perror("connect child");
         exit(-1);
     }
 
-    // TODO: need to talk about this
-    // make sure the server reports out after accept
-    ts.tv_sec=0;
-    ts.tv_nsec=010000000; // 10 ms
-    nanosleep(&ts, NULL);
+    // Wait for the server to tell us it's ready
+    if (sem_wait(sem) == -1) {
+        perror("Client:sem_wait2");
+        exit(-1);
+    }
 
     if (verbose > 0) fprintf(stdout, "Child sending: %s\n", test_data);
 
@@ -215,12 +258,15 @@ client_child() {
         exit(-1);
     }
 
-    // make sure the server sees this connection before we close
-    ts.tv_sec=0;
-    ts.tv_nsec=010000000; // 10 ms
-    nanosleep(&ts, NULL);
+    // Let the server report on this end before we close
+    if (sem_wait(sem) == -1) {
+        perror("Client:sem_wait3");
+        exit(-1);
+    }
 
+    fprintf(stdout, "Child closing and exiting\n");
     close(clientsock);
+    sem_close(sem);
     exit(0);
 }
 
@@ -229,6 +275,7 @@ int main(int argc, char **argv) {
     pid_t client, server;
     ino_t tx_lnode, tx_rnode, rx_lnode, rx_rnode;
     char *events;
+    sem_t *sem;
 
     if (argc < 2) {
         usage(argv[0]);
@@ -242,15 +289,22 @@ int main(int argc, char **argv) {
         }
     }
 
+    if ((sem = sem_open(SEMNAME, O_CREAT | O_RDWR, 0666, 0)) == SEM_FAILED) {
+        perror("Parent:sem_open");
+        exit(-1);
+    }
+
+    if (sem_init(sem, 1, 0) == -1) {
+        perror("Parent:sem_init");
+        exit(-1);
+    }
+
     // Note: we use procs so that events are flushed reliably on proc exit
     // start the server child
     if ((server = fork()) == 0) {
         //We are the child proc
         server_child();
     }
-
-    struct timespec ts = {.tv_sec=0, .tv_nsec=100000000}; // 100 ms
-    nanosleep(&ts, NULL);
 
     // start the client child
     if ((client = fork()) == 0) {
@@ -283,6 +337,8 @@ int main(int argc, char **argv) {
     if (unlink(EVENTFILE) == -1) {
         perror("unlink:check_event");
     }
+
+    sem_close(sem);
 
     if ((rc == 0) && (WEXITSTATUS(client_stat) == 0) && (WEXITSTATUS(server_stat) == 0)) {
         exit(0);

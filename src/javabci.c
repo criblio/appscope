e3791cfde0c0944337ee7613fa18b9c5db484666
Janusz Sobolewski <sobolewsk@gmail.com>
2020-06-16 13:02:26 +0200

    clean up BCI code

--- src/javabci.c
+++ src/javabci.c
@@ -13,121 +13,131 @@
 
 static char magic[] = { 0xca, 0xfe, 0xba, 0xbe };
 
+// constant pool entry sizes in bytes
 static char cp_tag_size[] = {
-  5,  // 3,
-  5,  // 4,
-  9,  // 5,
-  9,  // 6,
-  3,  // 7,
-  3,  // 8,
-  5,  // 9,
-  5,  // 10, 
-  5,  // 11,
-  5,  // 12,
+  0,  // CONSTANT_Utf8
   0,
   0,
-  4,  // 15,
-  3,  // 16,
-  5,  // 17,
-  5,  // 18,
-  3,  // 19,
-  3,  // 20  
+  5,  // CONSTANT_Integer,
+  5,  // CONSTANT_Float,
+  9,  // CONSTANT_Long,
+  9,  // CONSTANT_Double,
+  3,  // CONSTANT_Class,
+  3,  // CONSTANT_String,
+  5,  // CONSTANT_Fieldref,
+  5,  // CONSTANT_Methodref, 
+  5,  // CONSTANT_InterfaceMethodref,
+  5,  // CONSTANT_NameAndType,
+  0,
+  0,
+  4,  // CONSTANT_MethodHandle,
+  3,  // CONSTANT_MethodType,
+  5,  // CONSTANT_Dynamic,
+  5,  // CONSTANT_InvokeDynamic,
+  3,  // CONSTANT_Module,
+  3,  // CONSTANT_Package  
 };
 
-uint8_t readUInt8(void *addr) {
-  return *((uint8_t *)(addr));
-}
-
-uint16_t readUInt16(void *addr) {
-  return be16toh(*((uint16_t *)addr));
-}
-
-uint32_t readUInt32(void *addr) {
-  return be32toh(*((uint32_t *)addr));
-}
-
-char* javaGetUtf8String(java_class_t *info, int tagIndex) {
-  cp_info_t *cp = info->constant_pool[tagIndex - 1];
-  uint16_t len = readUInt16(cp + 1);
+char* 
+javaGetUtf8String(java_class_t *info, int tagIndex) 
+{
+  unsigned char *cp = info->constant_pool[tagIndex - 1];
+  uint16_t len = be16toh(*((uint16_t *)(cp + 1)));
   char *buf = malloc(len + 1);
   memcpy(buf, cp + 3, len);
   buf[len] = 0;
   return buf;
 }
 
-uint16_t javaGetTagLength(void *addr) {
+uint16_t 
+javaGetTagLength(unsigned char *addr) 
+{
   uint16_t len = 0;
-  uint8_t tag = readUInt8(addr);
+  uint8_t tag = *((uint8_t *)(addr));
   if (tag == CONSTANT_Utf8) {
-    len = readUInt16(addr + 1) + 3;
+    len = be16toh(*((uint16_t *)(addr + 1))) + 3;
   } else {
-    len = cp_tag_size[tag - 3];
+    len = cp_tag_size[tag];
   }
   return len;
 }
 
-uint16_t addTag(java_class_t *info, cp_info_t *tag) {
+static uint16_t 
+addTag(java_class_t *info, unsigned char *tag) 
+{
   uint16_t idx = info->constant_pool_count - 1;
   info->constant_pool_count++;
   info->constant_pool[idx] = tag;
   return idx + 1;
 }
 
-uint16_t addUtf8Tag(java_class_t *info, const char *str) {
+static uint16_t 
+addUtf8Tag(java_class_t *info, const char *str) 
+{
   size_t len = strlen(str);
   size_t bufsize = len + 3;
-  cp_info_t *utf8Tag = malloc(bufsize);
-  utf8Tag->tag = CONSTANT_Utf8;
+  unsigned char *utf8Tag = malloc(bufsize);
+  *((uint8_t *)utf8Tag)        = CONSTANT_Utf8;
   *((uint16_t *)(utf8Tag + 1)) = htobe16(len);
   memcpy(utf8Tag + 3, str, len);
   info->length += bufsize;
   return addTag(info, utf8Tag);
 }
 
-uint16_t javaAddNameAndTypeTag(java_class_t *info, const char *name, const char *desc) {
+uint16_t 
+javaAddNameAndTypeTag(java_class_t *info, const char *name, const char *desc) 
+{
   uint16_t nameIndex = addUtf8Tag(info, name);
   uint16_t descIndex = addUtf8Tag(info, desc);
   size_t bufsize = 5;
-  cp_info_t *tag = malloc(bufsize);
-  tag->tag = CONSTANT_NameAndType;
+  unsigned char *tag = malloc(bufsize);
+  *((uint8_t *)tag)        = CONSTANT_NameAndType;
   *((uint16_t *)(tag + 1)) = htobe16(nameIndex);
   *((uint16_t *)(tag + 3)) = htobe16(descIndex);
   info->length += bufsize;
   return addTag(info, tag);
 }
 
-uint16_t javaAddMethodRefTag(java_class_t *info, uint16_t classIndex, uint16_t nameAndTypeIndex) {
+uint16_t 
+javaAddMethodRefTag(java_class_t *info, uint16_t classIndex, uint16_t nameAndTypeIndex) 
+{
   size_t bufsize = 5;
-  cp_info_t *tag = malloc(bufsize);
-  tag->tag = CONSTANT_Methodref;
+  unsigned char *tag = malloc(bufsize);
+  *((uint8_t *)tag)        = CONSTANT_Methodref;
   *((uint16_t *)(tag + 1)) = htobe16(classIndex);
   *((uint16_t *)(tag + 3)) = htobe16(nameAndTypeIndex);
   info->length += bufsize;
   return addTag(info, tag);
 }
 
-uint32_t getAttributesLength(void *addr) {
-  uint16_t attr_count = readUInt16(addr);
-  void *off = addr + 2;
+static uint32_t 
+getAttributesLength(unsigned char *addr) 
+{
+  uint16_t attr_count = be16toh(*((uint16_t *)addr));
+  unsigned char *off = addr + 2;
   for (int j=0;j<attr_count;j++) {
-    uint32_t attr_length = readUInt32(off + 2);
+    uint32_t attr_length = be32toh(*((uint32_t *)(off + 2)));
     off += attr_length + 6;
   }
   return (uint32_t)(off - addr);
 }
 
-uint32_t javaGetMethodLength(void *addr) {
+uint32_t 
+javaGetMethodLength(unsigned char *addr) 
+{
   return getAttributesLength(addr + 6) + 6;
 }
 
-void * getCodeAttributeAddress(java_class_t *info, void *method) {
-  uint16_t attributes_count = readUInt16(method + 6);
+unsigned char *
+getCodeAttributeAddress(java_class_t *info, unsigned char *method) 
+{
+  uint16_t attributes_count = be16toh(*((uint16_t *)(method + 6)));
   
-  void *code = NULL;
-  void *off = method + 8;
+  unsigned char *code = NULL;
+  unsigned char *off = method + 8;
   for (int j=0;j<attributes_count && code == NULL;j++) {
-    uint16_t attr_name_index = readUInt16(off);
-    uint32_t attr_length     = readUInt32(off + 2);
+    uint16_t attr_name_index = be16toh(*((uint16_t *)off));
+    uint32_t attr_length     = be32toh(*((uint32_t *)(off + 2)));
     char *attr_name          = javaGetUtf8String(info, attr_name_index);
     if (strcmp(attr_name, "Code")==0) {
       code = off;
@@ -139,21 +149,25 @@ void * getCodeAttributeAddress(java_class_t *info, void *method) {
   return code;
 }
 
-uint16_t addStringTag(java_class_t *info, const char* str) {
+uint16_t 
+javaAddStringTag(java_class_t *info, const char* str)
+{
   uint16_t idx = addUtf8Tag(info, str);
-  cp_info_t *stringTag = malloc(3);
-  stringTag->tag = CONSTANT_String;
-  *((uint16_t *)(stringTag + 1)) = htobe16(idx);
-  return addTag(info, stringTag);
+  unsigned char *tag = malloc(3);
+  *((uint8_t *)tag)        = CONSTANT_String;
+  *((uint16_t *)(tag + 1)) = htobe16(idx);
+  return addTag(info, tag);
 }
 
-int javaFindClassIndex(java_class_t *info, const char *className) {
+int 
+javaFindClassIndex(java_class_t *info, const char *className) 
+{
   int idx = -1;
   for(int i=1;i<info->constant_pool_count;i++) {
-    cp_info_t *cp_info = info->constant_pool[i - 1];
-    uint8_t tag = cp_info->tag;
+    unsigned char *cp_info = info->constant_pool[i - 1];
+    uint8_t tag = *((uint8_t *)cp_info);
     if(tag == CONSTANT_Class) {
-      uint16_t name_index = readUInt16(cp_info + 1);
+      uint16_t name_index = be16toh(*((uint16_t *)(cp_info + 1)));
       char *name = javaGetUtf8String(info, name_index);
       if (strcmp(name, className) == 0) {
         idx = i;
@@ -165,12 +179,14 @@ int javaFindClassIndex(java_class_t *info, const char *className) {
   return idx;
 }
 
-int javaFindMethodIndex(java_class_t *info, const char *method, const char *signature) {
+int 
+javaFindMethodIndex(java_class_t *info, const char *method, const char *signature) 
+{
   int idx = -1;
   for (int i=0;i<info->methods_count;i++) {
-    void *addr = info->methods[i];
-    uint16_t name_index       = readUInt16(addr + 2);
-    uint16_t descriptor_index = readUInt16(addr + 4);
+    unsigned char *addr = info->methods[i];
+    uint16_t name_index       = be16toh(*((uint16_t *)(addr + 2)));
+    uint16_t descriptor_index = be16toh(*((uint16_t *)(addr + 4)));
     
     char *method_name = javaGetUtf8String(info, name_index);
     char *method_desc = javaGetUtf8String(info, descriptor_index);
@@ -185,9 +201,11 @@ int javaFindMethodIndex(java_class_t *info, const char *method, const char *sign
   return idx;
 }
 
-void javaCopyMethod(java_class_t *info, void *method, const char *newName) {
+void 
+javaCopyMethod(java_class_t *info, unsigned char *method, const char *newName) 
+{
   uint32_t len = javaGetMethodLength(method);
-  void *dest = malloc(len);
+  unsigned char *dest = malloc(len);
   memcpy(dest, method, len);
   uint16_t nameIndex = addUtf8Tag(info, newName);
    *((uint16_t *)(dest + 2)) = htobe16(nameIndex);
@@ -196,16 +214,18 @@ void javaCopyMethod(java_class_t *info, void *method, const char *newName) {
   info->length += len;
 }
 
-void javaConvertMethodToNative(java_class_t *info, int methodIndex) {
-  void *methodAddr = info->methods[methodIndex];
+void 
+javaConvertMethodToNative(java_class_t *info, int methodIndex) 
+{
+  unsigned char *methodAddr = info->methods[methodIndex];
   uint32_t len     = javaGetMethodLength(methodAddr);
   size_t bufsize   = 8;
-  void *addr       = malloc(bufsize);
+  unsigned char *addr       = malloc(bufsize);
   info->methods[methodIndex] = addr;
 
   memcpy(addr, methodAddr, bufsize);
 
-  uint16_t accessFlags     = readUInt16(methodAddr); 
+  uint16_t accessFlags     = be16toh(*((uint16_t *)methodAddr));
   uint16_t attributesCount = 0;
 
   *((uint16_t *)addr)        = htobe16(accessFlags | ACC_NATIVE | ACC_PUBLIC);
@@ -214,9 +234,10 @@ void javaConvertMethodToNative(java_class_t *info, int methodIndex) {
   info->length += bufsize - len;
 }
 
-void javaAddMethod(java_class_t *info, const char* name, const char* descriptor,  uint16_t accessFlags, 
-  uint16_t maxStack, uint16_t maxLocals, uint8_t *code, uint32_t codeLen) {
-
+void 
+javaAddMethod(java_class_t *info, const char* name, const char* descriptor, 
+              uint16_t accessFlags, uint16_t maxStack, uint16_t maxLocals, uint8_t *code, uint32_t codeLen) 
+{
   size_t codeAttrLen = 12 + codeLen;
   size_t bufsize = 8 + 6 + codeAttrLen;
 
@@ -225,7 +246,7 @@ void javaAddMethod(java_class_t *info, const char* name, const char* descriptor,
     bufsize = 8;
   }
 
-  void *addr = malloc(bufsize);
+  unsigned char *addr = malloc(bufsize);
   info->methods_count++;
   info->methods[info->methods_count - 1] = addr;
 
@@ -249,34 +270,38 @@ void javaAddMethod(java_class_t *info, const char* name, const char* descriptor,
     //write code attribute
     uint16_t exceptionLen = 0;
     uint16_t codeAttrCount = 0;
-    *((uint16_t *)addr) = htobe16(codeNameIndex);    addr += 2;
-    *((uint32_t *)addr) = htobe32(codeAttrLen);      addr += 4;
-    *((uint16_t *)addr) = htobe16(maxStack);         addr += 2;
-    *((uint16_t *)addr) = htobe16(maxLocals);        addr += 2;
-    *((uint32_t *)addr) = htobe32(codeLen);          addr += 4;
-    memcpy(addr, code, codeLen);                     addr += codeLen;
-    *((uint16_t *)addr) = htobe16(exceptionLen);     addr += 2;
+    *((uint16_t *)addr) = htobe16(codeNameIndex);  addr += 2;
+    *((uint32_t *)addr) = htobe32(codeAttrLen);    addr += 4;
+    *((uint16_t *)addr) = htobe16(maxStack);       addr += 2;
+    *((uint16_t *)addr) = htobe16(maxLocals);      addr += 2;
+    *((uint32_t *)addr) = htobe32(codeLen);        addr += 4;
+    memcpy(addr, code, codeLen);                   addr += codeLen;
+    *((uint16_t *)addr) = htobe16(exceptionLen);   addr += 2;
     *((uint16_t *)addr) = htobe16(codeAttrCount);    
   }
 
   info->length += bufsize;
 }
 
-void javaInjectCode(java_class_t *classInfo, void *method, uint8_t *code, size_t len) {
-  void *codeAttr = getCodeAttributeAddress(classInfo, method);
-  uint32_t codeLen = readUInt32(codeAttr + 10);
+void 
+javaInjectCode(java_class_t *classInfo, unsigned char *method, uint8_t *code, size_t len) 
+{
+  unsigned char *codeAttr = getCodeAttributeAddress(classInfo, method);
+  uint32_t codeLen = be32toh(*((uint32_t *)(codeAttr + 10)));
   memset(codeAttr + 14, 0, codeLen - 1);
   memcpy(codeAttr + 14, code, len);
 } 
 
-void javaWriteClass(void *dest, java_class_t *info) {
-  void *addr = dest;
-  memcpy(addr, magic, 4);                                     addr += 4;
+void 
+javaWriteClass(unsigned char *dest, java_class_t *info) 
+{
+  unsigned char *addr = dest;
+  memcpy(addr, magic, sizeof(magic));                         addr += sizeof(magic);
   *((uint16_t *)addr) = htobe16(info->minor_version);         addr += 2;
   *((uint16_t *)addr) = htobe16(info->major_version);         addr += 2;
   *((uint16_t *)addr) = htobe16(info->constant_pool_count);   addr += 2;
   for(int i=0;i<info->constant_pool_count - 1;i++) {
-    cp_info_t *cp = info->constant_pool[i];
+    unsigned char *cp = info->constant_pool[i];
     uint16_t size = javaGetTagLength(cp);
     memcpy(addr, cp, size);
     addr += size;
@@ -289,7 +314,7 @@ void javaWriteClass(void *dest, java_class_t *info) {
 
   *((uint16_t *)addr) = htobe16(info->fields_count); addr += 2;
   for (int i=0;i<info->fields_count;i++) {
-    void *field = info->fields[i];
+    unsigned char *field = info->fields[i];
     uint32_t size = getAttributesLength(field + 6) + 6;
     memcpy(addr, info->fields[i], size);
     addr += size;
@@ -297,7 +322,7 @@ void javaWriteClass(void *dest, java_class_t *info) {
   *((uint16_t *)addr) = htobe16(info->methods_count); addr += 2;
   //write methods
   for (int i=0;i<info->methods_count;i++) {
-    void *method = info->methods[i];
+    unsigned char *method = info->methods[i];
     uint32_t size = javaGetMethodLength(method);
     memcpy(addr, info->methods[i], size);
     addr += size;
@@ -305,62 +330,65 @@ void javaWriteClass(void *dest, java_class_t *info) {
   //write attributes
   *((uint16_t *)addr) = htobe16(info->attributes_count); addr += 2;
   for (int i=0;i<info->attributes_count;i++) {
-    void *attr = info->attributes[i];
-    uint32_t attribute_length = readUInt32(attr + 2); 
+    unsigned char *attr = info->attributes[i];
+    uint32_t attribute_length = be32toh(*((uint32_t *)(attr + 2)));
     size_t size = attribute_length + 6;
     memcpy(addr, info->attributes[i], size);
     addr += size;
   }
 }
 
-java_class_t* javaReadClass(void *buf) {
+java_class_t* 
+javaReadClass(const unsigned char* classData) 
+{
   java_class_t *classInfo = malloc(sizeof(java_class_t));
-  if (memcmp(buf, magic, sizeof(magic)) != 0) {
+  if (memcmp(classData, magic, sizeof(magic)) != 0) {
     return NULL;
   }
-  void *off = buf + 4;
-  classInfo->minor_version       = readUInt16(off); off += 2;
-  classInfo->major_version       = readUInt16(off); off += 2;
-  classInfo->constant_pool_count = readUInt16(off); off += 2;
+  unsigned char *addr = (unsigned char *)classData;
+  unsigned char *off = addr + sizeof(magic);
+  classInfo->minor_version        = be16toh(*((uint16_t *)off)); off += 2;
+  classInfo->major_version        = be16toh(*((uint16_t *)off)); off += 2;
+  classInfo->constant_pool_count  = be16toh(*((uint16_t *)off)); off += 2;
   classInfo->_constant_pool_count = classInfo->constant_pool_count;
-  classInfo->constant_pool       = (cp_info_t **) calloc(100 + (classInfo->constant_pool_count - 1), sizeof(cp_info_t *));
+  classInfo->constant_pool        = (unsigned char **) calloc(100 + (classInfo->constant_pool_count - 1), sizeof(unsigned char *));
   for(int i=1;i<classInfo->constant_pool_count;i++) {
-    classInfo->constant_pool[i - 1] = (cp_info_t *)off;
+    classInfo->constant_pool[i - 1] = (unsigned char *)off;
     off += javaGetTagLength(off);
   }
 
-  classInfo->access_flags        = readUInt16(off); off += 2;
-  classInfo->this_class          = readUInt16(off); off += 2;
-  classInfo->super_class         = readUInt16(off); off += 2;
-  classInfo->interfaces_count    = readUInt16(off); off += 2;
-  classInfo->interfaces          = (uint16_t *)off;
+  classInfo->access_flags         = be16toh(*((uint16_t *)off)); off += 2;
+  classInfo->this_class           = be16toh(*((uint16_t *)off)); off += 2;
+  classInfo->super_class          = be16toh(*((uint16_t *)off)); off += 2;
+  classInfo->interfaces_count     = be16toh(*((uint16_t *)off)); off += 2;
+  classInfo->interfaces           = (uint16_t *)off;
   off += classInfo->interfaces_count * 2;
 
   //fields
-  classInfo->fields_count        = readUInt16(off); off += 2;
-  classInfo->fields              = (void **) calloc(classInfo->fields_count, sizeof(void *));
+  classInfo->fields_count         = be16toh(*((uint16_t *)off)); off += 2;
+  classInfo->fields               = (unsigned char **) calloc(classInfo->fields_count, sizeof(unsigned char *));
   for (int i=0;i<classInfo->fields_count;i++) {
     classInfo->fields[i] = off;
     off += getAttributesLength(off + 6) + 6;
   }
 
-  classInfo->methods_count       = readUInt16(off); off += 2;
-  classInfo->_methods_count      = classInfo->methods_count;
-  classInfo->methods             = (void **) calloc(100 + classInfo->methods_count, sizeof(void *));
+  classInfo->methods_count        = be16toh(*((uint16_t *)off)); off += 2;
+  classInfo->_methods_count       = classInfo->methods_count;
+  classInfo->methods              = (unsigned char **) calloc(100 + classInfo->methods_count, sizeof(unsigned char *));
   for (int i=0;i<classInfo->methods_count;i++) {
     classInfo->methods[i] = off;
     off += javaGetMethodLength(off);
   }
 
-  classInfo->attributes_count    = readUInt16(off); off += 2;
-  classInfo->attributes          = (void **) calloc(classInfo->attributes_count, sizeof(void *));
+  classInfo->attributes_count     = be16toh(*((uint16_t *)off)); off += 2;
+  classInfo->attributes           = (unsigned char **) calloc(classInfo->attributes_count, sizeof(unsigned char *));
 
   for (int i=0;i<classInfo->attributes_count;i++) {
     classInfo->attributes[i] = off;
-    uint32_t attribute_length = readUInt32(off + 2);
+    uint32_t attribute_length = be32toh(*((uint32_t *)(off + 2)));
     off += attribute_length + 6;
   }
-  classInfo->length = off - buf;
+  classInfo->length = off - addr;
   return classInfo;
 }
 
--- src/javabci.h
+++ src/javabci.h
@@ -2,7 +2,10 @@
 #define __JAVABCI_H__
 #include <stdint.h>
 
+// Set of simple utilities for Java byte code instrumentation based on 
+// https://docs.oracle.com/javase/specs/jvms/se14/html/index.html
 
+// constant pool tags
 #define CONSTANT_Utf8                 1
 #define CONSTANT_Integer              3
 #define CONSTANT_Float                4
@@ -25,6 +28,7 @@
 #define OP_ALOAD_1                    0x2b
 #define OP_INVOKEVIRTUAL              0xb6
 
+// access flags
 #define ACC_PUBLIC                    0x0001
 #define ACC_PRIVATE                   0x0002
 #define ACC_PROTECTED                 0x0004
@@ -39,28 +43,23 @@
 #define ACC_SYNTHETIC                 0x1000
 
 
-typedef struct { 
-  uint8_t tag;
-} cp_info_t;
-
-
 typedef struct {
   uint8_t        magic[4];
 	uint16_t       minor_version;
 	uint16_t       major_version;
 	uint16_t       constant_pool_count;
-  cp_info_t      **constant_pool;
+  unsigned char  **constant_pool;
   uint16_t       access_flags;
   uint16_t       this_class;
   uint16_t       super_class;
   uint16_t       interfaces_count;
   uint16_t       *interfaces;
   uint16_t       fields_count;
-  void           **fields;
+  unsigned char  **fields;
   uint16_t       methods_count;
-  void           **methods;
+  unsigned char  **methods;
   uint16_t       attributes_count;
-  void           **attributes;
+  unsigned char  **attributes;
   uint32_t       length;
 
   uint16_t       _constant_pool_count;
@@ -68,24 +67,25 @@ typedef struct {
 } java_class_t;
 
 
-java_class_t*   javaReadClass(void *buf);
-void            javaWriteClass(void *dest, java_class_t *info);
+java_class_t*   javaReadClass(const unsigned char* classData);
+void            javaWriteClass(unsigned char *dest, java_class_t *info);
 void            javaDestroy(java_class_t **classInfo);
 
 int             javaFindClassIndex(java_class_t *info, const char *className);
 int             javaFindMethodIndex(java_class_t *info, const char *method, const char *signature);
-void            javaCopyMethod(java_class_t *info, void *method, const char *newName);
+void            javaCopyMethod(java_class_t *info, unsigned char *method, const char *newName);
 void            javaAddMethod(java_class_t *info, const char* name, const char* descriptor, 
                               uint16_t accessFlags, uint16_t maxStack, uint16_t maxLocals, 
                               uint8_t *code, uint32_t codeLen);
-void            javaInjectCode(java_class_t *classInfo, void *method, uint8_t *code, size_t len);
+void            javaInjectCode(java_class_t *classInfo, unsigned char *method, uint8_t *code, size_t len);
 
+uint16_t        javaAddStringTag(java_class_t *info, const char* str);
 uint16_t        javaAddNameAndTypeTag(java_class_t *info, const char *name, const char *desc);
 uint16_t        javaAddMethodRefTag(java_class_t *info, uint16_t classIndex, uint16_t nameAndTypeIndex);
 
 char*           javaGetUtf8String(java_class_t *info, int tagIndex);
-uint16_t        javaGetTagLength(void *addr);
-uint32_t        javaGetMethodLength(void *addr);
+uint16_t        javaGetTagLength(unsigned char *addr);
+uint32_t        javaGetMethodLength(unsigned char *addr);
 void            javaConvertMethodToNative(java_class_t *info, int methodIndex);
 
 #endif // __JAVABCI_H__

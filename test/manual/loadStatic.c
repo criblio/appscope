c41929cc46e22e364ddb219c4d6d54c09eaf9f84
Donn <donn@cribl.io>
2020-06-17 15:17:03 -0500

    WIP: first working loadable static executable in loadStatic.c

--- test/manual/loadStatic.c
+++ test/manual/loadStatic.c
@@ -12,13 +12,20 @@
 #include <errno.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
+#include <alloca.h>
 #include <elf.h>
+#include <sys/auxv.h>
 
 #define USE_MMAP 1
 #define ROUND_DOWN(num, unit) ((num) & ~((unit) - 1))
 #define ROUND_UP(num, unit) (((num) + (unit) - 1) & ~((unit) - 1))
+#define AUX_ENT(id, val) \
+	do { \
+		elf_info[i++] = id; \
+		elf_info[i++] = val; \
+	} while (0)
 
-extern char **environ;
+static char *g_exec_file;
 
 void
 usage(char *prog) {
@@ -27,57 +34,43 @@ usage(char *prog) {
 }
 
 static int
-osGetPageProt(uint64_t addr)
+get_file_size(char *path)
 {
-    int prot = -1;
-    size_t len = 0;
-    char *buf = NULL;
+    int fd;
+    struct stat sbuf;
 
-    FILE *fstream = fopen("/proc/self/maps", "r");
-    if (fstream == NULL) return -1;
-
-    while (getline(&buf, &len, fstream) != -1) {
-        char *end = NULL;
-        errno = 0;
-        uint64_t addr1 = strtoull(buf, &end, 0x10);
-        if ((addr1 == 0) || (errno != 0)) {
-            if (buf) free(buf);
-            fclose(fstream);
-            return -1;
-        }
-
-        uint64_t addr2 = strtoull(end + 1, &end, 0x10);
-        if ((addr2 == 0) || (errno != 0)) {
-            if (buf) free(buf);
-            fclose(fstream);
-            return -1;
-        }
-
-        printf("addr 0x%lux addr1 0x%lux addr2 0x%lux\n", addr, addr1, addr2);
-
-        if ((addr >= addr1) && (addr <= addr2)) {
-            char *perms = end + 1;
-            printf("matched 0x%lx to 0x%lx-0x%lx\n\t%c%c%c",
-                   addr, addr1, addr2, perms[0], perms[1], perms[2]);
-
-            prot = 0;
-            prot |= perms[0] == 'r' ? PROT_READ : 0;
-            prot |= perms[1] == 'w' ? PROT_WRITE : 0;
-            prot |= perms[2] == 'x' ? PROT_EXEC : 0;
-            if (buf) free(buf);
-            break;
-        }
-
-        if (buf) {
-            free(buf);
-            buf = NULL;
-        }
-
-        len = 0;
+    if ((fd = open(path, O_RDONLY)) == -1) {
+        perror("get_file_size:open");
+        return -1;
     }
 
-    fclose(fstream);
-    return prot;
+    if (fstat(fd, &sbuf) == -1) {
+        perror("get_file_size:fstat");
+        return -1;
+    }
+
+    if (close(fd) == -1) {
+        perror("get_file_size:close");
+        return -1;        
+    }
+
+    return sbuf.st_size;
+}
+
+static int
+is_static(char *buf)
+{
+    int i;
+    Elf64_Ehdr *elf = (Elf64_Ehdr *)buf;
+    Elf64_Phdr *phead = (Elf64_Phdr *)&buf[elf->e_phoff];
+
+    for (i = 0; i < elf->e_phnum; i++) {
+        if ((phead[i].p_type == PT_DYNAMIC) || (phead[i].p_type == PT_INTERP)) {
+            return 0;
+        }
+    }
+
+    return 1;
 }
 
 static void
@@ -92,12 +85,54 @@ dump_elf(char *buf, size_t len)
     free(buf);
 #endif
 }
+
 static int
-map_segment(char *buf, Elf64_Phdr *phead, int map)
+load_sections(char *buf, char *addr, size_t mlen)
+{
+    int i;
+    Elf64_Word type;
+    Elf64_Xword flags, len;
+    Elf64_Ehdr *ehdr;
+    Elf64_Shdr *sections;
+    const char *sec_name;
+    const char *section_strtab = NULL;
+    char *laddr;
+    
+    ehdr = (Elf64_Ehdr *)buf;
+    sections = (Elf64_Shdr *)((char *)buf + ehdr->e_shoff);
+    section_strtab = (char *)buf + sections[ehdr->e_shstrndx].sh_offset;
+    
+    for (i = 0; i < ehdr->e_shnum; i++) {
+        flags = sections[i].sh_flags;
+        len = sections[i].sh_size;
+        type = sections[i].sh_type;
+        laddr = (char *)sections[i].sh_addr;
+        
+        if ((laddr >= addr) && (laddr <= (addr + mlen))) {
+            sec_name = section_strtab + sections[i].sh_name;
+
+            if ((type != SHT_NOBITS) && ((flags & SHF_ALLOC) || (flags & SHF_EXECINSTR))) {
+
+                memmove(laddr, &buf[sections[i].sh_offset], len);
+            } else if (type == SHT_NOBITS) {
+                memset(laddr, 0, len);
+            }
+
+            printf("%s:%d %s addr %p - %p\n", __FUNCTION__, __LINE__,
+                       sec_name, laddr, laddr + len);
+        }
+    }
+
+    return 0;
+}
+
+static int
+map_segment(char *buf, Elf64_Phdr *phead)
 {
     int prot;
     int pgsz = sysconf(_SC_PAGESIZE);
     void *addr;
+    char *laddr;
     
     if ((phead->p_vaddr == 0) || (phead->p_memsz <= 0) || (phead->p_flags == 0)) return -1;
 
@@ -106,27 +141,22 @@ map_segment(char *buf, Elf64_Phdr *phead, int map)
     prot |= (phead->p_flags & PF_W) ? PROT_WRITE : 0;
     prot |= (phead->p_flags & PF_X) ? PROT_EXEC : 0;
 
-    printf("%s:%d vaddr 0x%lx size 0x%lx\n", __FUNCTION__, __LINE__, phead->p_vaddr, (size_t)phead->p_memsz);
-    if (map == 1) {
-        if ((addr = mmap((void *)ROUND_DOWN(phead->p_vaddr, pgsz),
-                         (size_t)phead->p_memsz, prot | PROT_WRITE,
-                         MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED,
-                         -1, (off_t)NULL)) == MAP_FAILED) {
-            perror("load_segment:mmap");
-            return -1;
-        }
-    } else {
-        addr = (void *)phead->p_vaddr;
-        prot = osGetPageProt((uint64_t)addr);
-        if (mprotect((void *)ROUND_DOWN(phead->p_vaddr, pgsz), phead->p_memsz, prot | PROT_WRITE) == -1) {
-            perror("load_segment:mprotect");
-            return -1;
-        }
+    laddr = (char *)ROUND_DOWN(phead->p_vaddr, pgsz);
+    
+    printf("%s:%d vaddr 0x%lx size 0x%lx\n", __FUNCTION__, __LINE__,
+           phead->p_vaddr, (size_t)phead->p_memsz);
+
+    if ((addr = mmap(laddr, (size_t)phead->p_memsz,
+                     prot | PROT_WRITE,
+                     MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED,
+                     -1, (off_t)NULL)) == MAP_FAILED) {
+        perror("load_segment:mmap");
+        return -1;
     }
 
-    memmove(addr, &buf[phead->p_offset], phead->p_memsz);
+    load_sections(buf, (char *)phead->p_vaddr, (size_t)phead->p_memsz);
 
-    if (((prot & PROT_WRITE) == 0) && (mprotect((void *)ROUND_DOWN(phead->p_vaddr, pgsz), phead->p_memsz, prot) == -1)) {
+    if (((prot & PROT_WRITE) == 0) && (mprotect(laddr, phead->p_memsz, prot) == -1)) {
         perror("load_segment:mprotect");
         return -1;
     }
@@ -144,9 +174,7 @@ load_elf(char *buf)
 
     for (i = 0; i < pnum; i++) {
         if (phead[i].p_type == PT_LOAD) {
-            map_segment(buf, &phead[i], 1);
-        } else {
-            map_segment(buf, &phead[i], 0);
+            map_segment(buf, &phead[i]);
         }
     }
 
@@ -180,10 +208,14 @@ get_symbol(char *buf, char *sname)
         }
 
         if ((strtab != NULL) && (symtab != NULL)) break;
-        printf("section %s = 0x%lx, size = 0x%lx\n",
-               section_strtab + sections[i].sh_name,
+        printf("section %s type = %d flags = 0x%lx addr = 0x%lx-0x%lx, size = 0x%lx off = 0x%lx\n",
+               sec_name,
+               sections[i].sh_type,
+               sections[i].sh_flags,
                sections[i].sh_addr,
-               sections[i].sh_size);
+               sections[i].sh_addr + sections[i].sh_size,
+               sections[i].sh_size,
+               sections[i].sh_offset);
     }
 
     for (i=0; i < nsyms; i++) {
@@ -196,34 +228,88 @@ get_symbol(char *buf, char *sname)
 
   return symaddr;
 }
-#if 0
+
 static int
-set_vars(char *buf, char **vars)
+copy_strings(char *buf, uint64_t sp, int argc, char **argv, char **env)
 {
     int i;
-    char *sym;
+    unsigned long auxindex;
+    Elf64_Ehdr *elf;
+    Elf64_Phdr *phead;
+    char **spp = (char **)sp;
+    uint64_t cnt = (uint64_t)argc;
+    Elf64_Addr *elf_info;
+    
+    if (!buf || !spp || !argv || !*argv || !env || !*env) return -1;
 
-    if (!vars || !*vars) return -1;
-
-    for (i = 0; *vars; i++) {
-        if ((sym = (char *)get_symbol(buf, vars[i])) == 0) return -1;
+    elf = (Elf64_Ehdr *)buf;
+    phead = (Elf64_Phdr *)&buf[elf->e_phoff];
+    
+    // do argc
+    *spp++ = (char *)cnt;
+    
+    // do argv
+    for (i = 0; i < argc; i++) {
+        if (&argv[i] && spp) {
+            *spp++ = argv[i];
+        } else {
+            printf("%s:%d ERROR: arg entry is not correct\n", __FUNCTION__, __LINE__);
+            return -1;
+        }
     }
 
-    return 0;
-}
-#endif
-
-static int
-copy_strings(int argc, char **argv)
-{
-    int i;
-    char *evsrc = __environ;
-    char *evdst = get_symbol("__environ");
-
-    if (!evsrc || !evdst) return -1;
+    // end of argv
+    *spp++ = NULL;
     
-    for (i = 0; evsrc; i++) {
-        
+    // do env
+    for (i = 0; env[i]; i++) {
+        if ((&env[i]) && spp) {
+            *spp++ = env[i];
+        } else {
+            printf("%s:%d ERROR: environ string is not correct\n", __FUNCTION__, __LINE__);
+            return -1;
+        }
+    }
+
+    // end of env
+    *spp++ = NULL;
+
+    elf_info = (Elf64_Addr *)spp;
+    memset(elf_info, 0, sizeof(Elf64_Addr) * (AT_EXECFN + 1) * 2);
+
+    for (auxindex = 1, i = 0; auxindex < 32; auxindex++) {
+        unsigned long val;
+
+        switch (auxindex) {
+        case AT_PHDR:
+            AUX_ENT(auxindex, (Elf64_Addr)phead);
+            break;
+
+        case AT_PHNUM:
+            AUX_ENT(auxindex, elf->e_phnum);
+            break;
+
+        case AT_BASE:
+            AUX_ENT(auxindex, -1);
+            break;
+
+        case AT_ENTRY:
+            AUX_ENT(auxindex, elf->e_entry);
+            break;
+
+        case AT_EXECFN:
+            AUX_ENT(auxindex, (unsigned long)g_exec_file);
+            break;
+
+        case AT_EXECFD:
+            //AUX_ENT(auxindex, );
+            break;
+
+        default:
+            if ((val = getauxval(auxindex)) != 0) {
+                AUX_ENT(auxindex, val);
+            }
+        }
     }
 
     return 0;
@@ -231,71 +317,41 @@ copy_strings(int argc, char **argv)
    
 //The first six integer or pointer arguments are passed in registers RDI, RSI, RDX, RCX, R8, R9
 static int
-set_go(Elf64_Ehdr *ehdr, int argc, char **argv)
+set_go(char *buf, int argc, char **argv, char **env)
 {
-    //uint64_t val = (uint64_t)(argv) + (sizeof(uint64_t) * argc);
-    //uint64_t val2 = (uint64_t)(argv) + (sizeof(uint64_t) * (argc + 1));
-    size_t val = sizeof(uint64_t) * argc;
-    size_t val2 = sizeof(uint64_t) * (argc + 1);
     uint64_t res;
-    //char *sfunc = (char *)ehdr->e_entry;
-    //int (*statexec)() = (int (*)())ehdr->e_entry;
+    char *sp, *heap;
+    Elf64_Ehdr *ehdr = (Elf64_Ehdr *)buf;
 
-    // src,dst
-    //asm volatile ("movl (%0), %%eax" :: "r" (argc));
-    //asm volatile ("movw %0, %%ds" :: "r" (ctxt->ds));
-    //asm volatile("movl (%1),%%eax" : "=a" (val) : "r" (pos));
+    // create a heap
+    if ((heap = mmap(NULL, (size_t)(200 * 1024),
+                     PROT_READ | PROT_WRITE,
+                     MAP_PRIVATE | MAP_ANONYMOUS,
+                     -1, (off_t)NULL)) == MAP_FAILED) {
+        perror("set_go:mmap");
+        return -1;
+    }
 
-    //asm volatile ("push %rsi");
-    //asm volatile ("push %rdx");
+    // create a stack
+    if ((sp = mmap(NULL, (size_t)(1024 * 1024) + (8 * 1024),
+                   PROT_READ | PROT_WRITE,
+                   MAP_PRIVATE | MAP_ANONYMOUS | MAP_GROWSDOWN,
+                   -1, (off_t)NULL)) == MAP_FAILED) {
+        perror("set_go:mmap");
+        return -1;
+    }
+
+    // build the stack and change the heap
+    copy_strings(buf, (uint64_t)sp, argc, argv, env);
+    brk(heap);
     
-    //     "orw $2, %%ax \n"
-    // "mov %1, %%rax \n"
-    //     "push %%rax \n"
-    //     : "%rax"         /* clobbered register */
-
-
-    //__asm__ volatile ("mov %1, 0(%%rsp) \n" : "=r"(res) : "r"(argc));
-
-
     __asm__ volatile (
-    "mov %1, 0(%%rsp) \n"
-    : "=r"(res) /* output */
-    : "r"(argc)      /* input */
-    );
-
-    __asm__ volatile (
-    "mov %1, 8(%%rsp) \n"
-    : "=r"(res) /* output */
-    : "r"(argv)      /* input */
-    );
-
-    __asm__ volatile (
-    "mov %%rsp, %%rax \n"
-    "add %1, %%rax \n"
-    "movl $0, 0(%%rax) \n"
-    : "=r"(res) /* output */
-    : "r"(val)      /* input */
-    : "%rax"         /* clobbered register */
-    );
-
-    __asm__ volatile (
-    "mov %%rsp, %%rax \n"
-    "add %1, %%rax \n"
-    "mov %2, 0(%%rax) \n"
-    : "=r"(res) /* output */
-    : "r"(val2), "r"(*environ)      /* input */
-    : "%rax"         /* clobbered register */
-    );
-
-    //statexec();
-    //__asm__ volatile ("jmp %0 \n" : : "r"((uint64_t)ehdr->e_entry));
-    __asm__ volatile (
-    "mov %1, %%rax \n"
-    "jmp *%%rax \n"
-    : "=r"(res) /* output */
-    : "r"(ehdr->e_entry)
-    : "%rax"         /* clobbered register */
+        "mov %1, %%r12 \n"
+        "mov %2, %%rsp \n"
+        "jmp *%%r12 \n"
+        : "=r"(res) /* output */
+        : "r"(ehdr->e_entry), "r"(sp)
+        : "%rax"         /* clobbered register */
     );
 
     return 0;
@@ -324,7 +380,6 @@ get_elf(char *path)
         perror("mmap:get_elf");
         return NULL;
     }
-
 #else
     if ((buf = calloc(1, sbuf.st_size)) == NULL) {
         perror("calloc:get_elf");
@@ -337,6 +392,7 @@ get_elf(char *path)
         return NULL;        
     }
 #endif
+
     if (close(fd) == -1) {
         perror("close:get_elf");
         dump_elf(buf, sbuf.st_size);
@@ -344,8 +400,12 @@ get_elf(char *path)
     }
 
     elf = (Elf64_Ehdr *)buf;
-    if((elf->e_ident[0] != 0x7f) || strncmp((char *)&elf->e_ident[1], "ELF", 3)) {
-        printf("%s:%d ERROR: %s is not an ELF file\n", __FUNCTION__, __LINE__, path);
+    if((elf->e_ident[EI_MAG0] != 0x7f) ||
+       strncmp((char *)&elf->e_ident[EI_MAG1], "ELF", 3) ||
+       (elf->e_ident[EI_CLASS] != ELFCLASS64) ||
+       (elf->e_ident[EI_DATA] != ELFDATA2LSB) ||
+       (elf->e_machine != EM_X86_64)) {
+        printf("%s:%d ERROR: %s is not a viable ELF file\n", __FUNCTION__, __LINE__, path);
         dump_elf(buf, sbuf.st_size);
         return NULL;
     }
@@ -357,14 +417,13 @@ get_elf(char *path)
     }
 
     return buf;
-
 }
 
 int
-main(int argc, char **argv)
+main(int argc, char **argv, char **env)
 {
-    int opt, verbose = 0;
-    char *path, *buf;
+    int opt, verbose = 0, flen;
+    char *buf;
     Elf64_Ehdr *ehdr;
     //int (*statexec)(int, char **);
 
@@ -379,16 +438,28 @@ main(int argc, char **argv)
     while ((opt = getopt(argc, argv, "vhf:")) > 0) {
         switch (opt) {
         case 'v': verbose++; break;
-        case 'f': path = optarg; break;
+        case 'f': g_exec_file = optarg; break;
         case 'h': default: usage(argv[0]); break;
         }
     }
     
-    printf("%s:%d loading %s\n", __FUNCTION__, __LINE__, path);
+    printf("%s:%d loading %s\n", __FUNCTION__, __LINE__, g_exec_file);
 
-    if ((buf = get_elf(path)) == NULL) {
+    if ((buf = get_elf(g_exec_file)) == NULL) {
         printf("%s:%d ERROR: read_file\n", __FUNCTION__, __LINE__);
         exit(-1);
+    }
+
+    if (!is_static(buf)) {
+        printf("%s:%d not a static file\n", __FUNCTION__, __LINE__);
+
+        if (fork() == 0) {
+            execve(g_exec_file, argv, env);
+            perror("execve");
+            exit(-1);
+        }
+
+        exit(0);
     }
 
     ehdr = (Elf64_Ehdr *)buf;
@@ -397,10 +468,13 @@ main(int argc, char **argv)
     load_elf(buf);
     printf("%s:%d sym @ 0x%lx\n", __FUNCTION__, __LINE__, get_symbol(buf, "__environ"));
 
-    set_go(ehdr, argc, argv);
+    set_go(buf, argc, argv, env);
 
-    //statexec = (int (*)(int, char **))ehdr->e_entry;
-    //statexec(0, NULL);
-    //dump_elf(buf, sbuf.st_size);
+    if ((flen = get_file_size(g_exec_file)) == -1) {
+        printf("%s:%d ERROR: file size\n", __FUNCTION__, __LINE__);
+        exit(-1);
+    }
+
+    dump_elf(buf, flen);
     return 0;
 }

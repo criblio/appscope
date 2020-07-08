e6705d532fde863c87cdb21c36aae46500759826
Janusz Sobolewski <sobolewsk@gmail.com>
2020-07-08 15:21:27 +0200

    more UT

--- test/manual/JavaTest.java
+++ test/manual/JavaTest.java
@@ -4,6 +4,8 @@ class JavaTest {
   public static final String staticField = "static";
   private String field1;
   public Integer field2;
+  private Double doubleField;
+  private Long   longField;
 
   void print(String str) {
     System.out.println(str);
@@ -14,6 +16,11 @@ class JavaTest {
   public static void main (String args[]) {
     JavaTest test = new JavaTest();
     test.print("Hello World");
+  }
+
+  void testDoubleAndLong() {
+    this.doubleField = 42.42;
+    this.longField = (long)42;
   }
 
 }

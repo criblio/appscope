e90c6c5bffa9dcd53b5473573e98ec2ed3a35503
Donn <donn@cribl.io>
2020-07-10 16:43:42 -0500

    Forgot to add the header file.

---
+++ src/gocontext.h
@@ -0,0 +1,12 @@
+#ifndef __GOTCONTEXT_H__
+#define __GOTCONTEXT_H__
+
+#define SYSCALL_WRITE 0
+#define SYSCALL_READ 1
+#define SYSCALL_OPEN 2
+#define SYSCALL_CLOSE 3
+#define SYSCALL_SOCKET 4
+#define SYSCALL_BIND 5
+#define SYSCALL_SENDTO 6
+
+#endif // __GOTCONTEXT_H__

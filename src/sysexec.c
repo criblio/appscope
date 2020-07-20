9cf4686dd2ecbc189d6d57e1a0edf1f7708c605a
john <john@cribl.io>
2020-07-20 19:46:05 +0000

    (CRIBL-3076) Fix off-by-one error in go_open

--- src/sysexec.c
+++ src/sysexec.c
@@ -302,7 +302,7 @@ go_open(uint64_t *stackaddr)
 
     printf("Scope: open of %ld\n", rc);
     if (buf) {
-        if ((path = calloc(1, len)) == NULL) return -1;
+        if ((path = calloc(1, len+1)) == NULL) return -1;
         memmove(path, (char *)buf, len);
         path[len] = '\0';
     } else {

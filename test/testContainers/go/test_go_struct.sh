098c031d9f96a233938541a2ee5ecfab9f07c4ba
john <john@cribl.io>
2020-08-21 22:14:15 -0500

    (CRIBL-3378) Remove duplicate table from test_go_struct.sh.

--- test/testContainers/go/test_go_struct.sh
+++ test/testContainers/go/test_go_struct.sh
@@ -5,8 +5,9 @@ FAILURE_COUNT=0
 # Capture a dump of the .zdebug_info from the elf file
 # This contains debug info which describes structures and the offsets
 # of their fields.
-STRUCT_FILE=readelf.dump
-readelf --debug-dump=info net/tlsServer > $STRUCT_FILE
+GO_APP=`pwd`/net/tlsServer
+APP_STRUCT_FILE=`pwd`/readelf.dump
+readelf --debug-dump=info $GO_APP > $APP_STRUCT_FILE
 
 # What a structure entry looks like in this readelf output:
 #      <1><66239>: Abbrev Number: 37 (DW_TAG_structure_type)
@@ -23,43 +24,11 @@ readelf --debug-dump=info net/tlsServer > $STRUCT_FILE
 #         <66263>   Unknown AT value: 2903: 0
 
 
-function check_expected_file_arg() {
-
-  if [ -z $1 ]; then
-      echo "test_go_struct.sh is missing a required arg; the path to an expected structure file"
-      echo "Example contents of this file:"
-      echo " runtime.g|m=48"
-      echo " runtime.m|tls=96"
-      echo " net/http.connReader|conn=0"
-      echo " net/http.conn|tlsState=48"
-      exit 1
-  fi
-
-  STRUCT_PATH=$1
-
-  if [ ! -f "$STRUCT_PATH" ]; then
-      echo "test_go_struct.sh cannot access $STRUCT_PATH"
-      echo "Example contents of $STRUCT_PATH:"
-      echo " runtime.g|m=48"
-      echo " runtime.m|tls=96"
-      echo " net/http.connReader|conn=0"
-      echo " net/http.conn|tlsState=48"
-      exit 1
-  fi
-}
-
-function test_it() {
+function test_structure() {
 
   STRUCT_NAME=$1
   FIELD_NAME=$2
-
-  FIELD_OFFSET=$(cat $STRUCT_PATH | grep "$STRUCT_NAME|$FIELD_NAME" | cut -d= -f2)
-
-  if [ -z "$FIELD_OFFSET" ]; then
-      echo "  Failed to find expected offset value '$STRUCT_NAME|$FIELD_NAME' in $STRUCT_PATH"
-      FAILURE_COUNT=$(($FAILURE_COUNT + 1))
-      return 1
-  fi
+  FIELD_OFFSET=$3
 
   # We want to find the line number of the start of our structure
   # and the line number of the start of the next structure
@@ -71,7 +40,7 @@ function test_it() {
   #      --
   #      69543: <1><3f8c5>: Abbrev Number: 37 (DW_TAG_structure_type)
   #      69544-    <3f8c6>   DW_AT_name        : runtime.stack
-  STRUCT_LINE_NUMS=$(grep -A1 -n DW_TAG_structure_type $STRUCT_FILE | grep -B1 -A3 ": ${STRUCT_NAME}$")
+  STRUCT_LINE_NUMS=$(grep -A1 -n DW_TAG_structure_type $APP_STRUCT_FILE | grep -B1 -A3 ": ${STRUCT_NAME}$")
 
   if [ -z "$STRUCT_LINE_NUMS" ]; then
       echo "  Failed to find structure '${STRUCT_NAME}'"
@@ -85,7 +54,7 @@ function test_it() {
   LAST_LINE_NUM=$(echo "$STRUCT_LINE_NUMS" | sed -n '4p' | cut -d: -f1)
 
   # This captures the text of the whole structure
-  STRUCT_RAW_OUT=$(cat $STRUCT_FILE | sed -n "${FIRST_LINE_NUM},${LAST_LINE_NUM}p")
+  STRUCT_RAW_OUT=$(cat $APP_STRUCT_FILE | sed -n "${FIRST_LINE_NUM},${LAST_LINE_NUM}p")
 
   # This captures the text of the whole field out of the structure
   FIELD_OUT=$(echo "$STRUCT_RAW_OUT" | grep -B1 -A3 ": ${FIELD_NAME}$")
@@ -109,19 +78,51 @@ function test_it() {
 }
 
 
+function check_expected_file_arg() {
+  if [ -z $1 ]; then
+      echo "test_go_struct.sh is missing a required arg; the path to an expected structure file"
+      echo "Example contents of this file:"
+      echo " runtime.g|m=48"
+      echo " runtime.m|tls=96"
+      echo " net/http.connReader|conn=0"
+      echo " net/http.conn|tlsState=48"
+      exit 1
+  fi
+
+  SCOPE_STRUCT_FILE=$1
+
+  if [ ! -f "$SCOPE_STRUCT_FILE" ]; then
+      echo "test_go_struct.sh cannot access $SCOPE_STRUCT_FILE"
+      echo "Example contents of $SCOPE_STRUCT_FILE:"
+      echo " runtime.g|m=48"
+      echo " runtime.m|tls=96"
+      echo " net/http.connReader|conn=0"
+      echo " net/http.conn|tlsState=48"
+      exit 1
+  fi
+}
+
+
+function test_each_line() {
+  check_expected_file_arg $1
+
+  echo "Using $SCOPE_STRUCT_FILE as the list of what structure offsets scope uses"
+  echo "Using $APP_STRUCT_FILE from $GO_APP as the source of truth"
+
+  while read LINE; do
+      echo "Evaluating $LINE"
+      #format example:"runtime.g|m=48"
+      STRUCT_NAME=$(echo $LINE | cut -d'|' -f1)
+      FIELD_NAME=$(echo $LINE | cut -d'|' -f2 | cut -d= -f1)
+      FIELD_OFFSET=$(echo $LINE | cut -d= -f2)
+
+      test_structure $STRUCT_NAME $FIELD_NAME $FIELD_OFFSET
+  done <$SCOPE_STRUCT_FILE
+}
+
 
 echo "executing test_go_struct.sh"
-check_expected_file_arg $1
-
-# The data we're interested in...
-# This is a complete list of the offsets we're using in scope library for go
-#
-#       STRUCT_NAME                   FIELD_NAME
-#       DW_TAG_structure_type         DW_TAG_member
-test_it "runtime.g"                   "m"
-test_it "runtime.m"                   "tls"
-test_it "net/http.connReader"         "conn"
-test_it "net/http.conn"               "tlsState"
+test_each_line $1
 
 if [ $FAILURE_COUNT -eq 0 ]; then
     echo "test_go_struct.sh PASSED"

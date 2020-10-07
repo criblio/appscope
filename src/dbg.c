caf1f0597d19732562323617596ef79467f79f26
john <john@cribl.io>
2020-10-07 16:21:21 +0000

    (CRIBL-3591)  Remove spinlock from dbg.c

--- src/dbg.c
+++ src/dbg.c
@@ -10,27 +10,26 @@
 
 
 #define MAX_INSTANCES_PER_LINE 2
-#define DEFAULT_NUM_LINES 256
+#define MAX_NUM_LINES 256
 
 typedef struct {
     time_t time;
     int err; // errno
-    char* str;
+    char *str;
 } occurrence_t;
 
 typedef struct {
-    const char* key;
-    unsigned long long count;
+    const char *key;
+    uint64_t count;
     occurrence_t instance[MAX_INSTANCES_PER_LINE];
 } line_t;
 
 struct _dbg_t {
-    line_t** lines;
-    unsigned max_lines;
+    line_t lines[MAX_NUM_LINES];
 };
 
-dbg_t* g_dbg = NULL;
-log_t* g_log = NULL;
+dbg_t *g_dbg = NULL;
+log_t *g_log = NULL;
 proc_id_t g_proc = {0};
 
 
@@ -39,52 +38,59 @@ dbgInit()
 {
     dbgDestroy();
 
-    g_dbg = calloc(1, sizeof(dbg_t));
-    if (!g_dbg) return;
-    g_dbg->max_lines = DEFAULT_NUM_LINES;
+    g_dbg = calloc(1, sizeof(*g_dbg));
+}
 
+static char *
+atomicSwapString(char **ptr, char *str)
+{
+    // This helper function just exists to hide all the type casting here
+    // instead of in the calling code.
+    char *prev_str;
+    do {
+        prev_str = *ptr;
+    } while (!atomicCasU64((uint64_t*)ptr, (uint64_t)prev_str, (uint64_t)str));
+    return prev_str;
 }
 
 static void
-updateLine(line_t* line, char* str)
+updateLine(line_t *line, char *str)
 {
     if (!line) return;
 
+    // Increment count atomically in a way so we know the original count.
+    // That's so the determination of i below is unique when possible.
+    uint64_t orig_count;
+    uint64_t new_count;
+    do {
+        orig_count = line->count;
+        new_count = orig_count + 1;
+    } while (!atomicCasU64(&line->count, orig_count, new_count));
+
     // This keeps overwriting the latest one.
-    int i = (line->count < MAX_INSTANCES_PER_LINE )
-            ? line->count
+    int i = (orig_count < MAX_INSTANCES_PER_LINE )
+            ? orig_count
             : MAX_INSTANCES_PER_LINE - 1;
 
     line->instance[i].time = time(NULL);
     line->instance[i].err = errno;
-    if (line->instance[i].str) free(line->instance[i].str);
-    line->instance[i].str = str;
-    line->count++;
-}
 
-static line_t*
-createLine(const char* key, char* str)
-{
-    if (!key) return NULL;
-    line_t* line = calloc(1, sizeof(line_t));
-    if (!line) return NULL;
-
-    line->key = key;
-    updateLine(line, str);
-
-    return line;
+    // The atomic swap allows us to free previous strings without leaking
+    // memory and equally importantly, ensures we can't double-free a str.
+    char *prev_str = atomicSwapString(&line->instance[i].str, str);
+    if (prev_str) free(prev_str);
 }
 
 static void
-destroyLine(line_t** line)
+resetLine(line_t *line)
 {
-    if (!line || !*line) return;
+    if (!line) return;
     int i;
     for (i=0; i < MAX_INSTANCES_PER_LINE; i++) {
-        if ((*line)->instance[i].str) free((*line)->instance[i].str);
+        char *prev_str = atomicSwapString(&line->instance[i].str, NULL);
+        if (prev_str) free(prev_str);
     }
-    free(*line);
-    *line = NULL;
+    memset(line, 0, sizeof(*line));
 }
 
 void
@@ -92,11 +98,10 @@ dbgDestroy()
 {
      if (!g_dbg) return;
 
-     if (g_dbg->lines) {
-        int i = 0;
-        while (g_dbg->lines[i]) destroyLine(&g_dbg->lines[i++]);
-        free(g_dbg->lines);
-    }
+     int i;
+     for (i=0; i < MAX_NUM_LINES; i++) {
+         resetLine(&g_dbg->lines[i]);
+     }
 
     free(g_dbg);
     g_dbg = NULL;
@@ -106,21 +111,24 @@ dbgDestroy()
 unsigned long long
 dbgCountAllLines()
 {
-    if (!g_dbg || !g_dbg->lines) return 0ULL;
+    if (!g_dbg) return 0ULL;
 
     unsigned long long i;
-    for (i=0; i<g_dbg->max_lines && g_dbg->lines[i]; i++);
+    for (i=0; i < MAX_NUM_LINES; i++) {
+        if (!g_dbg->lines[i].key) break;
+    }
     return i;
 }
 
 unsigned long long
-dbgCountMatchingLines(const char* str)
+dbgCountMatchingLines(const char *str)
 {
-    if (!g_dbg || !g_dbg->lines || !str) return 0ULL;
+    if (!g_dbg || !str) return 0ULL;
     unsigned long long result = 0ULL;
-    unsigned long long i;
-    for (i=0; i<g_dbg->max_lines && g_dbg->lines[i]; i++) {
-        if (strstr(g_dbg->lines[i]->key, str)) {
+    int i;
+    for (i=0; i < MAX_NUM_LINES; i++) {
+        if (!g_dbg->lines[i].key) break;
+        if (strstr(g_dbg->lines[i].key, str)) {
             result++;
         }
     }
@@ -128,7 +136,7 @@ dbgCountMatchingLines(const char* str)
 }
 
 static void
-dbgOutputHeaderLine(FILE* f)
+dbgOutputHeaderLine(FILE *f)
 {
     if (!f) return;
 
@@ -141,21 +149,22 @@ dbgOutputHeaderLine(FILE* f)
 }
 
 void
-dbgDumpAll(FILE* f)
+dbgDumpAll(FILE *f)
 {
     if (!f) return;
 
     dbgOutputHeaderLine(f);
 
-    if (!g_dbg || !g_dbg->lines) return;
+    if (!g_dbg) return;
 
     int i;
-    for (i=0; i<g_dbg->max_lines && g_dbg->lines[i]; i++) {
-        line_t* l = g_dbg->lines[i];
+    for (i=0; i < MAX_NUM_LINES; i++) {
+        if (!g_dbg->lines[i].key) break;
+        line_t *l = &g_dbg->lines[i];
 
         int j;
         for (j=0; j<MAX_INSTANCES_PER_LINE; j++) {
-            occurrence_t* o = &l->instance[j];
+            occurrence_t *o = &l->instance[j];
 
             struct tm t;
             char t_str[64];
@@ -164,7 +173,7 @@ dbgDumpAll(FILE* f)
             if (!strftime(t_str, sizeof(t_str), "%s", &t)) continue;
 
             if (j==0) {
-                fprintf(f, "%llu: %s %s %d(%s) %s\n",
+                fprintf(f, "%lu: %s %s %d(%s) %s\n",
                     l->count, l->key,
                     t_str, o->err, strerror(o->err), o->str);
             } else {
@@ -177,57 +186,56 @@ dbgDumpAll(FILE* f)
 
 // Setters
 static void
-dbgAddLineHelper(const char* const key, char* str)
+dbgAddLineHelper(const char * const key, char *str)
 {
-    if (!g_dbg->lines) {
-        g_dbg->lines = calloc(1, sizeof(line_t*) * g_dbg->max_lines);
-        if (!g_dbg->lines) return;
-    }
+    line_t *line;
+    int i;
 
-    // See if the line is already there
-    line_t* line = NULL;
-    int i = 0;
-    while (g_dbg->lines[i]) {
+search_for_spot:
+    line = NULL;
+
+    // See if a line already has our key
+    for (i=0; i < MAX_NUM_LINES; i++) {
+        if (!g_dbg->lines[i].key) break;
+
         // We don't have to do a strcmp; comparing the pointers works
         // since the key is generated by compiler
-        if (key == g_dbg->lines[i]->key) {
-            line = g_dbg->lines[i];
+        if (key == g_dbg->lines[i].key) {
+            line = &g_dbg->lines[i];
             break;
         }
-        i++;
     }
 
-    // The line is already there, just update it
+    // A line already has our key, just update it
     if (line) {
         updateLine(line, str);
         return;
     }
 
-    // If we're out of space, try to get more space
-    if (i >= g_dbg->max_lines-1) {     // null delimiter is always required
-        int tmp_max_lines = g_dbg->max_lines * 2;  // double each time
-        line_t** temp = realloc(g_dbg->lines, sizeof(line_t*) * tmp_max_lines);
-        if (!temp) {
-            if (str) free(str);
-            return;
-        }
-        // Yeah!  We have more space!  init it, and set our state to remember it
-        memset(&temp[g_dbg->max_lines], 0, sizeof(line_t*) * (tmp_max_lines - g_dbg->max_lines));
-        g_dbg->lines = temp;
-        g_dbg->max_lines = tmp_max_lines;
+    // If we're out of space for our key, give up.
+    if (i >= MAX_NUM_LINES) {
+        if (str) free(str);
+        return;
     }
 
-    // Create the new line
-    g_dbg->lines[i] = createLine(key, str);
+    // Assign our key to a line in a real-time safe way.
+    line = &g_dbg->lines[i];
+    if (atomicCasU64((uint64_t*)&line->key, 0ULL, (uint64_t)key)) {
+        updateLine(line, str);
+    } else {
+        // Another thread took the spot was thought was available.
+        // Starting the search over will ensure we don't add one key twice.
+        goto search_for_spot;
+    }
 }
 
 void
-dbgAddLine(const char* key, const char* fmt, ...)
+dbgAddLine(const char *key, const char *fmt, ...)
 {
     if (!g_dbg) return;
 
     // Create the string
-    char* str = NULL;
+    char *str = NULL;
     if (fmt) {
         va_list args;
         va_start(args, fmt);
@@ -239,15 +247,7 @@ dbgAddLine(const char* key, const char* fmt, ...)
         }
     }
 
-    // One at a time, guys, one at a time.
-    static uint64_t spinlock = 0ULL;
-    while (!atomicCasU64(&spinlock, 0ULL, 1ULL));
-
     dbgAddLineHelper(key, str);
-
-    // unleash the hounds!
-    atomicCasU64(&spinlock, 1ULL, 0ULL);
-
 }
 
 

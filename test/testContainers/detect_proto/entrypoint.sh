e42b0b7d5ea68d2760c1237615059b83659c2d95
Donn <donn@cribl.io>
2020-12-17 23:48:02 +0000

    Adjusting to the reality that centos 6 is no longer supported.

--- test/testContainers/detect_proto/entrypoint.sh
+++ test/testContainers/detect_proto/entrypoint.sh
@@ -5,7 +5,8 @@ mkdir /var/log/beats
 if [ "$1" = "start" ]; then
 #    sleep 5 && sh /sbin/loaddata.sh &
     redis-server --protected-mode no &
-    service mongod start
+    #    service mongod start
+    mongod --dbpath /var/lib/mongo --logpath /var/log/mongodb/mongod.log --fork
 
     /opt/test-runner/bin/test_protocols.sh
 #    tail -f /dev/null

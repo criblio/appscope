c903810e7c39feeada660d0caaa290b7c83fb8b7
Clint Sharp <clint@cribl.io>
2021-01-28 20:11:31 +0000

    another stab at deploy, should work this time

--- website/deploy.sh
+++ website/deploy.sh
@@ -8,9 +8,12 @@ npm ci
 npx gatsby build
 
 BUCKET=io.appscope.staging
+DISTRIBUTION_ID=E2O0IS8RABQ4AT
 
 if [ $GITHUB_REF == "refs/heads/master" ]; then
     BUCKET=io.appscope
+    DISTRIBUTION_ID=E3CI6UPKUT68NJ
 fi
 
-aws s3 cp ${DIR}/public s3://$BUCKET --recursive
+aws s3 cp ${DIR}/public s3://${BUCKET} --recursive
+aws cloudfront create-invalidation --distribution-id=${DISTRIBUTION_ID} --paths '/*'

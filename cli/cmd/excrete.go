adb7bc9f0e1b72822763e0070fd1d8e730a88f2a
Clint Sharp <clint@cribl.io>
2021-01-30 06:25:49 +0000

    renaming scopec to ldscope, adding excrete command, adding CICD

---
+++ cli/cmd/excrete.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/criblio/scope/run"
+	"github.com/criblio/scope/util"
+	"github.com/spf13/cobra"
+)
+
+// excreteCmd represents the excrete command
+var excreteCmd = &cobra.Command{
+	Use:     "excrete (<dir>)",
+	Aliases: []string{"birth", "expunge", "extricate"},
+	Short:   "Output instrumentary library files to <dir>",
+	Long: `Excrete outputs ldscope, libscope.so, scope.yml and scope_protocol.yml to the provided directory. These files can configured
+to instrument any application and output the data to any existing tool using simple TCP protocols. Libscope can easily be used
+with any dynamic or static application, regardless of the runtime.`,
+	Example: `scope extrete
+scope excrete /opt/libscope`,
+	Args: cobra.MaximumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		outPath := "./"
+		if len(args) > 0 {
+			if !util.CheckFileExists(args[0]) {
+				util.ErrAndExit("%s does not exist", args[0])
+			}
+			outPath = args[0]
+		}
+
+		err := run.CreateAll(outPath)
+		util.CheckErrSprintf(err, "error excreting files: %v", err)
+		fmt.Printf("Successfully excreted to %s.\n", outPath)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(excreteCmd)
+}

111ed111892fa93c1e13aef86c0d510f8936fff4
Clint Sharp <clint@cribl.io>
2021-02-01 17:31:12 +0000

    only deploy master builds to demo

--- cli/docker-demo.sh
+++ cli/docker-demo.sh
@@ -2,7 +2,9 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo $SCOPECI_TOKEN | docker login -u scopeci --password-stdin
-cd ${DIR}
-docker build -t cribl/scope-demo -f demo/Dockerfile .
-docker push cribl/scope-demo
+if [ $GITHUB_REF == "refs/heads/master" ]; then
+    echo $SCOPECI_TOKEN | docker login -u scopeci --password-stdin
+    cd ${DIR}
+    docker build -t cribl/scope-demo -f demo/Dockerfile .
+    docker push cribl/scope-demo
+fi

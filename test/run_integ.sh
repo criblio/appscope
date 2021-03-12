b88bb0446c5c52d6ffd7ab7640f4a7a84736049a
Jan Bieron <jbieron+github@gmail.com>
2021-03-12 14:43:25 +0000

    Two stage go images;
    + do not rebuild COPY /xxx stage if the only change was Dockerfile

--- test/run_integ.sh
+++ test/run_integ.sh
@@ -2,18 +2,15 @@
 set -e
 cd `dirname $0`/testContainers
 
-echo `env`
-
-# enabled="cribl detect_proto elastic gogen interposed_func kafka nginx splunk tls go_2 go_3 go_4 go_5 go_6 go_7 go_8 go_9 go_10 go_11 go_12 go_13 go_14 go_15 go_16"
-enabled="cribl detect_proto go_2 go_10"
+# working="cribl detect_proto elastic gogen interposed_func kafka nginx splunk tls"
+enabled="cribl detect_proto elastic gogen interposed_func kafka nginx splunk tls go_2 go_3 go_4 go_5 go_6 go_7 go_8 go_9 go_10 go_11 go_12 go_13 go_14 go_15 go_16"
 docker-compose pull -q $enabled
-docker-compose build $enabled
-# docker-compose push $enabled
-# # working="cribl detect_proto elastic gogen interposed_func kafka nginx splunk tls"
+docker-compose build --parallel $enabled
 for s in $enabled; do
     docker-compose up $s
 done
 
+docker-compose ps
 if docker-compose ps |tail -n+3|grep -v 'Exit 0'; then
     2>&1 echo Integration tests failed
     exit 1

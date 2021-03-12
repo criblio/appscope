e1eeb2934401a0c4d51300accaa64d02779d3dca
Jan Bieron <jbieron+github@gmail.com>
2021-03-12 13:38:46 +0000

    Pull first then build if necessary

--- test/run_integ.sh
+++ test/run_integ.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 set -e
-# working="cribl detect_proto elastic gogen interposed_func kafka nginx splunk tls"
-enabled="cribl detect_proto elastic gogen interposed_func kafka nginx splunk tls go_2 go_3 go_4 go_5 go_6 go_7 go_8 go_9 go_10 go_11 go_12 go_13 go_14 go_15 go_16"
 cd `dirname $0`/testContainers
-# docker-compose build $enabled
+
+echo `env`
+
+# enabled="cribl detect_proto elastic gogen interposed_func kafka nginx splunk tls go_2 go_3 go_4 go_5 go_6 go_7 go_8 go_9 go_10 go_11 go_12 go_13 go_14 go_15 go_16"
+enabled="cribl detect_proto go_2 go_10"
+docker-compose pull -q $enabled
+docker-compose build $enabled
+# docker-compose push $enabled
+# # working="cribl detect_proto elastic gogen interposed_func kafka nginx splunk tls"
 for s in $enabled; do
     docker-compose up $s
 done
 
 if docker-compose ps |tail -n+3|grep -v 'Exit 0'; then
-    2>&1 echo some containers failed
-    # exit 1
+    2>&1 echo Integration tests failed
+    exit 1
+fi
+
+if [ $GITHUB_REF = 'refs/heads/master' ]; then
+    docker-compose push
 fi

754295e236980f86456dfeea563269461b01d1c7
Clint Sharp <clint@cribl.io>
2021-04-01 04:56:41 +0000

    fixing deploy script

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,9 @@ fi fi fi
 
 TMPDIR=$(mktemp -d)
 mkdir -p ${TMPDIR}/scope
-cp ${DIR}/bin/linux/{scope,ldscope,libscope.so,scope.yml,scope_protocol.yml} ${TMPDIR}/scope/
+cp ${DIR}/bin/linux/{scope,ldscope} ${TMPDIR}/scope/
+cp ${DIR}/lib/linux/libscope.so ${TMPDIR}/scope/
+cp ${DIR}/conf/{scope.yml,scope_protocol.yml} ${TMPDIR}/scope/
 cd ${TMPDIR} && tar cfz scope.tgz scope
 cd scope && md5sum scope > scope.md5 && cd -
 md5sum scope.tgz > scope.tgz.md5

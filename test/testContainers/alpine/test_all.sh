6fe881cbc74a446ea4cc8739981ce140589d4b9a
Sean Valeo <seanvaleo@outlook.com>
2021-06-08 18:09:37 -0400

    build cli as a static executable

--- test/testContainers/alpine/test_all.sh
+++ test/testContainers/alpine/test_all.sh
@@ -10,4 +10,8 @@ export SCOPE_EVENT_DEST=file:///opt/test/logs/events.log
 /opt/test/bin/test_tls.sh
 ERR+=$?
 
+export SCOPE_EVENT_DEST=file:///opt/test/logs/events.log
+/opt/test/bin/test_scope.sh
+ERR+=$?
+
 exit ${ERR}
---
+++ test/testContainers/alpine/test_scope.sh
@@ -0,0 +1,81 @@
+#! /bin/bash
+
+DEBUG=0  # set this to 1 to capture the EVT_FILE for each test
+
+FAILED_TEST_LIST=""
+FAILED_TEST_COUNT=0
+
+EVT_FILE="/opt/test/logs/events.log"
+
+starttest(){
+    CURRENT_TEST=$1
+    echo "==============================================="
+    echo "             Testing $CURRENT_TEST             "
+    echo "==============================================="
+    ERR=0
+}
+
+evaltest(){
+    echo "             Evaluating $CURRENT_TEST"
+}
+
+endtest(){
+    if [ $ERR -eq "0" ]; then
+        RESULT=PASSED
+    else
+        RESULT=FAILED
+        FAILED_TEST_LIST+=$CURRENT_TEST
+        FAILED_TEST_LIST+=" "
+        FAILED_TEST_COUNT=$(($FAILED_TEST_COUNT + 1))
+    fi
+
+    echo "*************** $CURRENT_TEST $RESULT ***************"
+    echo ""
+    echo ""
+
+    # copy the EVT_FILE to help with debugging
+    if (( $DEBUG )) || [ $RESULT == "FAILED" ]; then
+        cp $EVT_FILE $EVT_FILE.$CURRENT_TEST
+    fi
+
+    rm $EVT_FILE
+    touch $EVT_FILE
+}
+
+
+################ BEGIN TESTS ################
+
+
+#
+# scope
+#
+starttest scope
+
+scope
+ERR+=$?
+
+evaltest
+
+endtest
+
+
+################ END TESTS ################
+
+
+unset SCOPE_PAYLOAD_ENABLE
+unset SCOPE_PAYLOAD_HEADER
+
+if (( $FAILED_TEST_COUNT == 0 )); then
+    echo ""
+    echo ""
+    echo "*************** ALL TESTS PASSED ***************"
+else
+    echo "*************** SOME TESTS FAILED ***************"
+    echo "Failed tests: $FAILED_TEST_LIST"
+    echo "Refer to these files for more info:"
+    for FAILED_TEST in $FAILED_TEST_LIST; do
+        echo "  $EVT_FILE.$FAILED_TEST"
+    done
+fi
+
+exit ${FAILED_TEST_COUNT}

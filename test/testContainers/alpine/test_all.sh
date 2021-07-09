09ec96a73027c96060f3aa32c64251e553afdc59
Paul Dugas <pdugas@cribl.io>
2021-07-09 10:49:44 -0400

    Moved Go test to last
    The alpine integration tests runs fine locally but often stalls when run
    at GitHub. This change is a guess that something leftover from the first
    Go test may be causing the stall.

--- test/testContainers/alpine/test_all.sh
+++ test/testContainers/alpine/test_all.sh
@@ -2,16 +2,16 @@
 
 ERR=0
 
-export SCOPE_EVENT_DEST=file:///go/events.log
-/go/test_go.sh
-ERR+=$?
-
 export SCOPE_EVENT_DEST=file:///opt/test/logs/events.log
 /opt/test/bin/test_tls.sh
 ERR+=$?
 
 export SCOPE_EVENT_DEST=file:///opt/test/logs/events.log
 /opt/test/bin/test_scope.sh
+ERR+=$?
+
+export SCOPE_EVENT_DEST=file:///go/events.log
+/go/test_go.sh
 ERR+=$?
 
 exit ${ERR}

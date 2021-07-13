ecb96f61740faf3fe603088a3eedff92184893f5
Paul Dugas <pdugas@cribl.io>
2021-07-12 20:21:46 -0400

    Checkpoint

--- test/testContainers/alpine/test_all.sh
+++ test/testContainers/alpine/test_all.sh
@@ -6,12 +6,12 @@ export SCOPE_EVENT_DEST=file:///opt/test/logs/events.log
 /opt/test/bin/test_tls.sh
 ERR+=$?
 
-#export SCOPE_EVENT_DEST=file:///opt/test/logs/events.log
-#/opt/test/bin/test_scope.sh
-#ERR+=$?
+export SCOPE_EVENT_DEST=file:///opt/test/logs/events.log
+/opt/test/bin/test_scope.sh
+ERR+=$?
 
-#export SCOPE_EVENT_DEST=file:///go/events.log
-#/go/test_go.sh
-#ERR+=$?
+export SCOPE_EVENT_DEST=file:///go/events.log
+/go/test_go.sh
+ERR+=$?
 
 exit ${ERR}

17bfc068dc5fe126708905440c0fda2e5a5288d8
Ubuntu <ubuntu@pi0.dugas.lan>
2021-07-28 15:46:54 -0400

    remove LD_PRELOAD from detect_proto integ test
    We're simplifying this test to focus on ensuring scoped Redis and
    MongoDB clients produce protocol detect events. There's not need to use
    LD_PRELOAD and scope everything.

--- test/testContainers/detect_proto/test_protocols.sh
+++ test/testContainers/detect_proto/test_protocols.sh
@@ -1,55 +1,69 @@
 #! /bin/bash
 
 declare -i ERR=0
-preload=`env | grep LD_PRELOAD`
 
+wait_for_port() {
+    TIMEOUT=${2:-60}
+    while ! netstat -an | grep -w ${1} >/dev/null 2>&1; do
+        sleep 1
+        ((TIMEOUT=TIMEOUT-1))
+        if [ $TIMEOUT -le 0 ]; then
+            echo >&2 "warn: timed out waiting for port ${1} listener"
+            netstat -an | grep -w LISTEN >&2
+            return
+        fi
+    done
+    echo 1
+}
+
+echo
 echo "==============================================="
 echo "             Testing Redis                     "
 echo "==============================================="
-
-redis-cli SET detect hello
-unset LD_PRELOAD
-
-grep remote_protocol /opt/test-runner/logs/events.log > /dev/null
-ERR+=$?
-
-grep '"protocol":"Redis"' /opt/test-runner/logs/events.log > /dev/null
-ERR+=$?
-
-
-if [ $ERR -eq "0" ]; then
-    echo "*************** Redis Success ***************"
+if [ "$(wait_for_port 6379)" ]; then
+	ldscope redis-cli SET detect hello
+	grep remote_protocol /opt/test-runner/logs/events.log > /dev/null
+	ERR+=$?
+	grep '"protocol":"Redis"' /opt/test-runner/logs/events.log > /dev/null
+	ERR+=$?
+	if [ $ERR -eq "0" ]; then
+		echo "*************** Redis Success ***************"
+	else
+		echo "*************** Redis Test Failed ***************"
+		cat /opt/test-runner/logs/events.log
+	fi
+	rm /opt/test-runner/logs/events.log
 else
-    echo "*************** Redis Test Failed ***************"
-#    cat /opt/test-runner/logs/events.log
+	ERR+=1
+	echo "*************** Redis Test Failed ***************"
 fi
-
-rm /opt/test-runner/logs/events.log
 
 if [ "x86_64" = "$(uname -m)" ]; then # x86_64 only
 
-echo "==============================================="
-echo "             Testing Mongo                     "
-echo "==============================================="
-
-export $preload
-mongo /opt/test-runner/bin/mongo.js
-unset LD_PRELOAD
-
-grep remote_protocol /opt/test-runner/logs/events.log > /dev/null
-ERR+=$?
-
-grep '"protocol":"Mongo"' /opt/test-runner/logs/events.log > /dev/null
-ERR+=$?
-
-if [ $ERR -eq "0" ]; then
-    echo "*************** Mongo Success ***************"
-else
-    echo "*************** Mongo Test Failed ***************"
-#    cat /opt/test-runner/logs/events.log
-fi
+	MONGO_ERR=0
+	echo
+	echo "==============================================="
+	echo "             Testing Mongo                     "
+	echo "==============================================="
+	if [ "$(wait_for_port 27017)" ]; then
+		ldscope mongo /opt/test-runner/bin/mongo.js
+		grep remote_protocol /opt/test-runner/logs/events.log > /dev/null
+		MONGO_ERR+=$?
+		grep '"protocol":"Mongo"' /opt/test-runner/logs/events.log > /dev/null
+		MONGO_ERR+=$?
+		if [ $MONGO_ERR -eq "0" ]; then
+			echo "*************** Mongo Success ***************"
+		else
+			echo "*************** Mongo Test Failed ***************"
+			cat /opt/test-runner/logs/events.log
+		fi
+		rm /opt/test-runner/logs/events.log
+		ERR+=$MONGO_ERR
+	else
+		ERR+=1
+		echo "*************** Mongo Test Failed ***************"
+	fi
 
 fi # x86_64 only
 
-rm /opt/test-runner/logs/events.log
 exit ${ERR}

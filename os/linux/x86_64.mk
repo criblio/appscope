e0d408aed2620e9b98487fb4e2c9b7c13a0ebb07
Paul Dugas <pdugas@cribl.io>
2021-07-31 16:00:04 -0400

    Checkpoint

--- os/linux/x86_64.mk
+++ os/linux/x86_64.mk
@@ -2,10 +2,6 @@ ARCH_CFLAGS=-D__GO__ -D__FUNCHOOK__ -D__x86_64__
 ARCH_LD_FLAGS=-lcapstone
 ARCH_BINARY=elf64-x86-64
 ARCH_OBJ=i386
-FUNCHOOK_AR=contrib/build/funchook/libfunchook.a contrib/build/funchook/capstone_src-prefix/src/capstone_src-build/libcapstone.a
-
-$(FUNCHOOK_AR):
-	@$(MAKE) -C contrib funchook
 
 LD_FLAGS=$(PCRE2_AR) -ldl -lpthread -lrt -Lcontrib/build/funchook -lfunchook -Lcontrib/build/funchook/capstone_src-prefix/src/capstone_src-build -lcapstone
 INCLUDES=-I./contrib/libyaml/include -I./contrib/cJSON -I./os/$(OS) -I./contrib/pcre2/src -I./contrib/build/pcre2 -I./contrib/build/funchook/capstone_src-prefix/src/capstone_src/include -I./contrib/jni -I./contrib/jni/linux/ -I./contrib/openssl/include -I./contrib/build/openssl/include
@@ -16,9 +12,9 @@ INCLUDES=-I./contrib/libyaml/include -I./contrib/cJSON -I./os/$(OS) -I./contrib/
 #	rm -f ./lib/$(OS)/libscope.so
 
 $(LIBSCOPE): src/wrap.c src/state.c src/httpstate.c src/report.c src/httpagg.c src/plattime.c src/fn.c os/$(OS)/os.c src/cfgutils.c src/cfg.c src/transport.c src/log.c src/mtc.c src/circbuf.c src/linklist.c src/evtformat.c src/ctl.c src/mtcformat.c src/com.c src/dbg.c src/search.c src/sysexec.c src/gocontext.S src/scopeelf.c src/wrap_go.c src/utils.c src/bashmem.c $(YAML_SRC) contrib/cJSON/cJSON.c src/javabci.c src/javaagent.c
-	$(MAKE) $(FUNCHOOK_AR)
-	$(MAKE) $(PCRE2_AR)
-	$(MAKE) $(OPENSSL_AR)
+	@$(MAKE) -C contrib funchook
+	@$(MAKE) -C contrib pcre2
+	@$(MAKE) -C contrib openssl
 	@echo "$${CI:+::group::}Building $@"
 	$(CC) $(CFLAGS) $(ARCH_CFLAGS) \
 		-shared -fvisibility=hidden -fno-stack-protector \

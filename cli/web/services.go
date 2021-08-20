3c0dd131eb5832c5f21eb76293d05e99de2ad8af
Paul Dugas <pdugas@cribl.io>
2021-08-20 11:19:53 -0400

    Checkpoint

--- cli/web/services.go
+++ cli/web/services.go
@@ -126,12 +126,26 @@ func groupSearchHandler(c *clients.Clients) gin.HandlerFunc {
 }
 
 func groupCreateHandler(c *clients.Clients) gin.HandlerFunc {
-    return func(ctx *gin.Context) {
-		ctx.JSON(http.StatusNotImplemented, gin.H{
-			"success": false,
-			"error": "Not implemented",
+	return func(ctx *gin.Context) {
+		var json clients.Group
+		if err := ctx.ShouldBindJSON(&json); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"success": false,
+				"error": err.Error(),
+			})
+			return
+		}
+
+		item := c.Groups.Create(json.Name, json.Config, json.Filters)
+
+		// TODO: Add Location header to response
+
+		ctx.JSON(http.StatusCreated, gin.H{
+			"success": true,
+			"len": 1,
+			"items": [1]clients.Group{*item},
 		})
-    }
+	}
 }
 
 func groupReadHandler(c *clients.Clients) gin.HandlerFunc {
@@ -164,27 +178,133 @@ func groupReadHandler(c *clients.Clients) gin.HandlerFunc {
 
 func groupUpdateHandler(c *clients.Clients) gin.HandlerFunc {
     return func(ctx *gin.Context) {
-		ctx.JSON(http.StatusNotImplemented, gin.H{
-			"success": false,
-			"error": "Not implemented",
+		id, err := strconv.Atoi(ctx.Param("id"))
+		if err != nil || id < 0 {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"success": false,
+				"error": "Invalid ID; " + err.Error(),
+			})
+			return
+		}
+
+		item, err := c.Groups.Read(uint(id))
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"success": false,
+				"error": err.Error(),
+			})
+			return
+		}
+
+		var json clients.Group
+		if err := ctx.ShouldBindJSON(&json); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"success": false,
+				"error": err.Error(),
+			})
+			return
+		}
+
+		// TODO: need a way to update item and save it instead of risking
+		// partial updates and having to re-Read at the end.
+		if err := c.Groups.Update(uint(id), json.Name); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"success": false,
+				"error": err.Error(),
+			})
+			return
+		}
+		if err := c.Groups.Update(uint(id), json.Config); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"success": false,
+				"error": err.Error(),
+			})
+			return
+		}
+		if err := c.Groups.Update(uint(id), json.Filters); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"success": false,
+				"error": err.Error(),
+			})
+			return
+		}
+		item, _ = c.Groups.Read(uint(id))
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"success": true,
+			"len": 1,
+			"items": [1]clients.Group{*item},
 		})
 	}
 }
 
 func groupDeleteHandler(c *clients.Clients) gin.HandlerFunc {
     return func(ctx *gin.Context) {
-		ctx.JSON(http.StatusNotImplemented, gin.H{
-			"success": false,
-			"error": "Not implemented",
+		id, err := strconv.Atoi(ctx.Param("id"))
+		if err != nil || id < 0 {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"success": false,
+				"error": "Invalid ID; " + err.Error(),
+			})
+			return
+		}
+
+		item, err := c.Groups.Read(uint(id))
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"success": false,
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if err := c.Groups.Delete(uint(id)); err != nil {
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"success": false,
+				"error": err.Error(),
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"success": true,
+			"len": 1,
+			"items": [1]clients.Group{*item},
 		})
     }
 }
 
 func groupApplyHandler(c *clients.Clients) gin.HandlerFunc {
     return func(ctx *gin.Context) {
-		ctx.JSON(http.StatusNotImplemented, gin.H{
-			"success": false,
-			"error": "Not implemented",
+		id, err := strconv.Atoi(ctx.Param("id"))
+		if err != nil || id < 0 {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"success": false,
+				"error": "Invalid ID; " + err.Error(),
+			})
+			return
+		}
+
+		_, err = c.Groups.Read(uint(id))
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"success": false,
+				"error": err.Error(),
+			})
+			return
+		}
+/*
+		if err := c.PushGroupConfig(id); err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"success": false,
+				"error": err.Error(),
+			})
+			return
+		}
+*/
+		ctx.JSON(http.StatusOK, gin.H{
+			"success": true,
+			// TODO list scopes that were applied? The new config?
 		})
 	}
 }

8eef147a0dc475f71f94279d1e7844d6b166175c
Paul Dugas <pdugas@cribl.io>
2021-09-10 14:55:57 +0000

    add UPX's lzma-sdk dependency

---
+++ contrib/upx/src/lzma-sdk/C/7zip/Compress/LZ/BinTree/BinTree2.h
@@ -0,0 +1,14 @@
+// BinTree2.h
+
+#ifndef __BINTREE2_H
+#define __BINTREE2_H
+
+#define BT_NAMESPACE NBT2
+#define BT_NO_HASH_MASK 1
+
+#include "BinTreeMain.h"
+
+#undef BT_NO_HASH_MASK
+#undef BT_NAMESPACE
+
+#endif

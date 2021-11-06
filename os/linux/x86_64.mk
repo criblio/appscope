a970489215d2fc9a3a7bf31db4bce790c1c0f1b2
michalbiesek <mbiesek@cribl.io>
2021-11-06 23:07:58 +0100

    Add Patchelf to build system
    - TODO: add call for patchelf as last step during build

--- os/linux/x86_64.mk
+++ os/linux/x86_64.mk
@@ -12,7 +12,7 @@ INCLUDES=-I./contrib/libyaml/include -I./contrib/cJSON -I./os/$(OS) -I./contrib/
 #	rm -f ./lib/$(OS)/libscope.so
 
 $(LIBSCOPE): src/wrap.c src/state.c src/httpstate.c src/report.c src/httpagg.c src/plattime.c src/fn.c os/$(OS)/os.c src/cfgutils.c src/cfg.c src/transport.c src/log.c src/mtc.c src/circbuf.c src/linklist.c src/evtformat.c src/ctl.c src/mtcformat.c src/com.c src/dbg.c src/search.c src/sysexec.c src/gocontext.S src/scopeelf.c src/wrap_go.c src/utils.c src/bashmem.c $(YAML_SRC) contrib/cJSON/cJSON.c src/javabci.c src/javaagent.c
-	@$(MAKE) -C contrib funchook pcre2 openssl ls-hpack
+	@$(MAKE) -C contrib funchook pcre2 openssl ls-hpack patchelf
 	@echo "$${CI:+::group::}Building $@"
 	$(CC) $(CFLAGS) $(ARCH_CFLAGS) \
 		-shared -fvisibility=hidden -fno-stack-protector \

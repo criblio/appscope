0a25f260708142f4514b864389bbc17ef7d2e528
michalbiesek <mbiesek@cribl.io>
2021-11-22 15:03:48 +0100

    Extend payload start message with gid and uid

--- src/com.c
+++ src/com.c
@@ -189,6 +189,8 @@ jsonProcessObject(proc_id_t *proc)
 
     if (!(cJSON_AddNumberToObjLN(root, "pid", proc->pid))) goto err;
     if (!(cJSON_AddNumberToObjLN(root, "ppid", proc->ppid))) goto err;
+    if (!(cJSON_AddNumberToObjLN(root, "gid", proc->gid))) goto err;
+    if (!(cJSON_AddNumberToObjLN(root, "uid", proc->uid))) goto err;
     if (!(cJSON_AddStringToObjLN(root, "hostname", proc->hostname))) goto err;
     if (!(cJSON_AddStringToObjLN(root, "procname", proc->procname))) goto err;
     if (proc->cmd) {

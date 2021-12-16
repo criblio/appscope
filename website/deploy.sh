525a567150a454323ba3249b66e7a5535887d5f6
john <john@cribl.io>
2021-12-16 16:38:05 +0000

    fix-website-build... try setting NVM_DIR and running nvm.sh script

--- website/deploy.sh
+++ website/deploy.sh
@@ -4,11 +4,23 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd ${DIR}
 
-# manage the version of node used
+# for debugging
+ls -al $HOME
+env
+
+# nvm is provided by the runner, but needs to be setup for this shell env
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# for debugging
+env
+
+# use nvm to control the version of node used
 echo "versions originally provided by the environment..."
 npm --version
 node --version
-nvm --version
+nvm --version | true
 nvm install 14.18.1
 nvm use 14.18.1
 echo "versions we're going to use..."

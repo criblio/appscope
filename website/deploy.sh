a8e9efa445b103c619a72917e9791f816d7dd3c8
john <john@cribl.io>
2021-12-16 18:18:38 +0000

    fix-website-build...  Got it working!  Cleanup the debug a bit.

--- website/deploy.sh
+++ website/deploy.sh
@@ -4,29 +4,23 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd ${DIR}
 
-# for debugging
-ls -al $HOME
-env
-
 # nvm is provided by the runner, but needs to be setup for this shell env
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm --version
 
-# for debugging
-env
+echo "npm and node versions originally provided..."
+npm --version
+node --version
 
 # use nvm to control the version of node used
-echo "versions originally provided by the environment..."
-npm --version
-node --version
-nvm --version | true
 nvm install 14.18.1
 nvm use 14.18.1
-echo "versions we're going to use..."
+
+echo "npm and node versions we're going to use..."
 npm --version
 node --version
-nvm --version
 
 npm ci
 npx gatsby build

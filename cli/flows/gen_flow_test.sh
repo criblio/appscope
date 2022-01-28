0e03966f4feb0a59dcdf568f142ba1fa95d6e78b
john <john@cribl.io>
2022-01-28 04:06:41 +0000

    (#730) schema for 1.0.0:  Binary encoded unit test data?  blech.

--- cli/flows/gen_flow_test.sh
+++ cli/flows/gen_flow_test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+GOBIN=$(pwd)/.gobin/x86_64
 
-scope run -p -- curl --http1.1 -so /dev/null https://cribl.io/
-cwd=$(pwd); cd $(scope hist -d) && go-bindata -pkg flows -o ${DIR}/flow_file_test.go -fs . payloads; cd -
+BINDIR=../bin/linux/x86_64
+SCOPE=${BINDIR}/scope
+
+GO_BINDATA=${GOBIN}/go-bindata
+
+$SCOPE run -p -- curl --http1.1 -so /dev/null https://cribl.io/
+cwd=$(pwd); cd $($SCOPE hist -d) && $GO_BINDATA -pkg flows -o ${DIR}/flow_file_test.go -fs . payloads; cd -

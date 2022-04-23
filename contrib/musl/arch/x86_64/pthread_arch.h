e304ed56524e9b102f64b8d93fb6dc8904bf18ac
Donn <donn@cribl.io>
2022-04-23 22:13:49 +0200

    [MUSL] Ensure that references to __ptheed_self(), albeit inadvertant, are safe.

--- contrib/musl/arch/x86_64/pthread_arch.h
+++ contrib/musl/arch/x86_64/pthread_arch.h
@@ -1,8 +1,12 @@
+static struct first_tls {
+	char c;
+    char pt[512];
+	void *space[16];
+} first_tls[1];
+
 static inline uintptr_t __get_tp()
 {
-	uintptr_t tp;
-	__asm__ ("mov %%fs:0,%0" : "=r" (tp) );
-	return tp;
+    return (uintptr_t)first_tls[0].pt;
 }
 
 #define MC_PC gregs[REG_RIP]

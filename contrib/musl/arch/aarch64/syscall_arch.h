8d93c41079b3db7c98e621a79d73672a377fe892
michalbiesek <mbiesek@cribl.io>
2022-04-25 10:23:57 +0200

    Revert "[MUSL] Remove VDSO logic"
    This reverts commit 9deacc916c1926dcc1680f515245d673489e0a99.

--- contrib/musl/arch/aarch64/syscall_arch.h
+++ contrib/musl/arch/aarch64/syscall_arch.h
@@ -71,5 +71,8 @@ static inline long __syscall6(long n, long a, long b, long c, long d, long e, lo
 	__asm_syscall("r"(x8), "0"(x0), "r"(x1), "r"(x2), "r"(x3), "r"(x4), "r"(x5));
 }
 
+#define VDSO_USEFUL
+#define VDSO_CGT_SYM "__kernel_clock_gettime"
+#define VDSO_CGT_VER "LINUX_2.6.39"
 
 #define IPC_64 0

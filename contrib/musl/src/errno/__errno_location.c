459e45d7f4b69512a0c392364248a69cbf39c533
Donn <donn@cribl.io>
2022-08-03 09:38:35 -0500

    Merging errno changes.

--- contrib/musl/src/errno/__errno_location.c
+++ contrib/musl/src/errno/__errno_location.c
@@ -3,11 +3,11 @@
 #include <unistd.h>
 #include "pthread_impl.h"
 
-int errno_val[1024 * 64];
+int errno_val;
 
 int *__errno_location(void)
 {
-	return &errno_val[gettid()];
+	return &errno_val;
 }
 
 weak_alias(__errno_location, ___errno_location);

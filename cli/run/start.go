25f29051ef2b7571613b69baa5c14f4519592d18
michalbiesek <mbiesek@cribl.io>
2022-09-02 14:03:52 +0200

    Describe scope start

--- cli/run/start.go
+++ cli/run/start.go
@@ -1,7 +1,6 @@
 package run
 
 import (
-	"errors"
 	"io"
 	"io/ioutil"
 	"os"
@@ -44,7 +43,8 @@ type allowProcConfig struct {
 // return status from start operation
 var startErr error
 
-// retrieve and unmarshall configuration passed in stdin
+// Retrieve and unmarshall configuration passed in stdin.
+// It returns the status of operation.
 func getConfigFromStdin() (startConfig, []byte, error) {
 
 	var startCfg startConfig
@@ -60,179 +60,267 @@ func getConfigFromStdin() (startConfig, []byte, error) {
 	return startCfg, data, err
 }
 
-func (rc *Config) startAttachStage(process util.Process, cfgData []byte) error {
-	if !process.Scoped {
-		pid := strconv.Itoa(process.Pid)
-		tmpFile, err := os.CreateTemp(".", pid)
-		if err != nil {
-			return err
-		}
-		defer os.Remove(tmpFile.Name())
-
-		if _, err := tmpFile.Write(cfgData); err != nil {
-			return err
-		}
-
-		if err := tmpFile.Close(); err != nil {
-			return err
-		}
-
-		env := append(os.Environ(), "SCOPE_CONF_PATH="+tmpFile.Name())
-		ld := loader.ScopeLoader{Path: ldscopePath()}
-		stdoutStderr, err := ld.AttachSubProc([]string{pid}, env)
-		if err != nil {
-			log.Error().Err(err).Msgf("Attach failed for PID %v details, %v", pid, stdoutStderr)
-		} else {
-			log.Debug().Msgf("Attach success")
-		}
-		os.Unsetenv("SCOPE_CONF_PATH")
+// startAttachSingleProcess attach to the the specific process identifed by pid with configuration pass in cfgData.
+// It returns the status of operation.
+func startAttachSingleProcess(pid string, cfgData []byte) error {
+	tmpFile, err := os.CreateTemp(".", pid)
+	if err != nil {
+		log.Error().
+			Err(err).
+			Str("pid", pid).
+			Msgf("Attach PID %v Failed. Create temporary attach configuration failed.", pid)
+		startErr = err
 		return err
 	}
-	return errAlreadyScope
+	defer os.Remove(tmpFile.Name())
+
+	if _, err := tmpFile.Write(cfgData); err != nil {
+		log.Error().
+			Err(err).
+			Str("pid", pid).
+			Msgf("Attach PID %v Failed. Write temporary attach configuration failed.", pid)
+		startErr = err
+		return err
+	}
+
+	env := append(os.Environ(), "SCOPE_CONF_PATH="+tmpFile.Name())
+	ld := loader.ScopeLoader{Path: ldscopePath()}
+	stdoutStderr, err := ld.AttachSubProc([]string{pid}, env)
+	if err != nil {
+		log.Error().
+			Err(err).
+			Str("pid", pid).
+			Str("loaderDetails", stdoutStderr).
+			Msgf("Attach PID %v Failed. Write temporary attach configuration failed.", pid)
+		startErr = err
+	} else {
+		log.Info().
+			Str("pid", pid).
+			Msgf("Attach PID %v success.", pid)
+	}
+
+	os.Unsetenv("SCOPE_CONF_PATH")
+	return err
 }
 
-func setupHost(filterData []byte) error {
+// startAttach attach to all allowed processes on the host and on the container
+// It returns the status of operation.
+func startAttach(alllowProc allowProcConfig) error {
+	cfgSingleProc, err := yaml.Marshal(alllowProc.Config)
+	if err != nil {
+		log.Error().
+			Err(err).
+			Msg("Attach Failed. Serialize configuration failed.")
+		startErr = err
+		return err
+	}
+
+	allProc, err := util.ProcessesByName(alllowProc.Procname)
+	if err != nil {
+		log.Error().
+			Err(err).
+			Str("process", alllowProc.Procname).
+			Msgf("Attach Failed. Retrieve process name: %v failed.", alllowProc.Procname)
+		startErr = err
+		return err
+	}
+
+	for _, process := range allProc {
+		if process.Scoped {
+			log.Info().
+				Str("pid", strconv.Itoa(process.Pid)).
+				Msgf("Attach Failed. Process: %v is already scoped.", process.Pid)
+			continue
+		}
+
+		startAttachSingleProcess(strconv.Itoa(process.Pid), cfgSingleProc)
+
+	}
+
+	return nil
+}
+
+// startConfigureHost setup the Host:
+//
+// - setup /etc/profile.d/scope.sh
+//
+// - extract filter into the filter file in /tmp/scope_filter.yml
+//
+// - extract libscope.so into the filter file in /tmp/libscope.so
+//
+// It returns the status of operation.
+func startConfigureHost(filterData []byte) error {
 	tempFile, err := ioutil.TempFile("/tmp", "temp_filter")
 	if err != nil {
+		log.Error().
+			Err(err).
+			Msg("Setup host failed. Create temporary filter file.")
+		startErr = err
 		return err
 	} else {
-		log.Debug().Msg("Create test_filter succeded.")
+		log.Info().
+			Msg("Setup host. Create test_filter success.")
 	}
 	defer os.Remove(tempFile.Name())
 
 	if _, err := tempFile.Write(filterData); err != nil {
+		log.Error().
+			Err(err).
+			Msg("Setup host failed. Write to temporary filter file.")
+		startErr = err
 		return err
 	} else {
-		log.Debug().Msg("Write test_filter succeded.")
+		log.Info().
+			Msg("Setup host. Write test_filter success.")
 	}
 
 	ld := loader.ScopeLoader{Path: ldscopePath()}
 
 	stdoutStderr, err := ld.ConfigureHost(tempFile.Name())
 	if err != nil {
-		os.Remove(tempFile.Name())
-		log.Debug().Msg("Setup on Host failed.")
-		log.Fatal().Err(err).Msgf("Configure Host failed details %v", stdoutStderr)
+		log.Error().
+			Err(err).
+			Str("loaderDetails", stdoutStderr).
+			Msg("Setup host failed.")
+		startErr = err
 		return err
 	} else {
-		log.Debug().Msgf("Setup on Host succeded %v", stdoutStderr)
+		log.Info().
+			Msg("Setup host success.")
 	}
 
 	return nil
 }
 
-func SetupContainer(allowProcs []allowProcConfig) error {
+// startServiceHost setup the Container:
+//
+// - setup service defined by the serviceName on the host.
+//
+// It returns the status of operation.
+func startServiceHost(serviceName string) error {
+	ld := loader.ScopeLoader{Path: ldscopePath()}
+	stdoutStderr, err := ld.ServiceHost(serviceName)
+	if err != nil {
+		log.Error().
+			Err(err).
+			Str("service", serviceName).
+			Str("loaderDetails", stdoutStderr).
+			Msgf("Service %v host failed.", serviceName)
+		startErr = err
+	} else {
+		log.Info().
+			Str("service", serviceName).
+			Msgf("Service %v host success.", serviceName)
+	}
+	return err
+}
+
+// startSetupContainer setup the Container:
+//
+// - setup /etc/profile.d/scope.sh in the container.
+//
+// - extract filter into the filter file in /tmp/scope_filter.yml in the container.
+//
+// - extract libscope.so into the filter file in /tmp/libscope.so in the container.
+//
+// - service services defined by the allowProcs process name list in the container.
+//
+// It returns the status of operation.
+func startSetupContainer(allowProcs []allowProcConfig) error {
 	// Iterate over all containers
-	cPids, _ := util.GetDockerPids()
+	cPids, err := util.GetDockerPids()
+	if err != nil {
+		log.Error().
+			Err(err).
+			Msg("Setup containers failed.")
+		startErr = err
+	}
+
 	for _, cPid := range cPids {
-		log.Debug().Msgf("Start set up namespace for Pid %d", cPid)
-
-		// Setup /etc/profile.d
-		// Extract filter file
-		// Extract libscope.so
-
 		ld := loader.ScopeLoader{Path: ldscopePath()}
 		stdoutStderr, err := ld.ConfigureContainer("/tmp/scope_filter.yml", cPid)
 		if err != nil {
-			log.Error().Err(err).Msgf("Setup container namespace failed for PID %v, details %v", cPid, stdoutStderr)
+			log.Error().
+				Err(err).
+				Str("pid", strconv.Itoa(cPid)).
+				Str("loaderDetails", stdoutStderr).
+				Msgf("Setup containers failed. Setup container %v failed.", cPid)
 			startErr = err
 		} else {
-			log.Debug().Msgf("Setup container succesfully PID: %v", cPid)
+			log.Info().
+				Str("pid", strconv.Itoa(cPid)).
+				Msgf("Setup containers. Setup container %v success.", cPid)
 		}
 
 		// Iterate over all allowed processses
 		for _, process := range allowProcs {
-			log.Debug().Msgf("Start setup service %v for container PID: %v", process.Procname, cPid)
 			// Setup service
 			stdoutStderr, err := ld.ServiceContainer(process.Procname, cPid)
 			if err != nil {
-				log.Error().Err(err).Msgf("Setup service %v failed for container PID: %v, details %v", process.Procname, cPid, stdoutStderr)
+				log.Error().
+					Err(err).
+					Str("service", process.Procname).
+					Str("pid", strconv.Itoa(cPid)).
+					Str("loaderDetails", stdoutStderr).
+					Msgf("Setup containers failed. Service %v container %v failed.", process.Procname, cPid)
 				startErr = err
 			} else {
-				log.Debug().Msgf("Setup service succesfully: %v for container PID: %v", process.Procname, cPid)
+				log.Info().
+					Str("service", process.Procname).
+					Str("pid", strconv.Itoa(cPid)).
+					Msgf("Setup containers. Service %v container %v success.", process.Procname, cPid)
 			}
 		}
 	}
 	return nil
 }
 
+// Run start command responsible for setup host and container env
+// It returns the status of operation.
 func (rc *Config) Start() error {
 	rc.setupWorkDir([]string{"start"}, true)
 
 	// Validate user has root permissions
 	if err := util.UserVerifyRootPerm(); err != nil {
-		log.Fatal().Err(err)
+		log.Fatal().
+			Err(err).
+			Msg("Verfy root permission failed.")
+		startErr = err
 		return err
 	}
 
+	// Retrieve input data
 	startCfg, startcfgData, err := getConfigFromStdin()
 	if err != nil {
-		log.Fatal().Err(err)
+		log.Fatal().
+			Err(err).
+			Msg("Read filter file failed.")
+		startErr = err
 		return err
 	}
 
 	if err := createLdscope(); err != nil {
-		log.Fatal().Err(err)
+		log.Fatal().
+			Err(err).
+			Msg("Create ldscope failed.")
+		startErr = err
 		return err
 	}
 
-	// Setup Host
-	if err := setupHost(startcfgData); err != nil {
-		log.Error().Err(err).Msgf("Setup host failed: %v", err)
-		startErr = err
-	} else {
-		log.Debug().Msgf("Setup host succesfully")
-	}
+	// Configure Host
+	startConfigureHost(startcfgData)
 
-	if err := SetupContainer(startCfg.AllowProc); err != nil {
-		log.Error().Err(err).Msgf("Setup container failed: %v", err)
-		startErr = err
-	} else {
-		log.Debug().Msgf("Setup container succesfully")
-	}
+	// Setup Containers
+	startSetupContainer(startCfg.AllowProc)
 
 	// Iterate over allowed process
-	// Attach to services on Host and container
 	// Setup service on Host
+	// Attach to services on Host and container
 	for _, alllowProc := range startCfg.AllowProc {
 
-		// Setup service
-		ld := loader.ScopeLoader{Path: ldscopePath()}
-		stdoutStderr, err := ld.ServiceHost(alllowProc.Procname)
-		if err != nil {
-			log.Error().Err(err).Msgf("Setup service %v failed for host, details %v", alllowProc.Procname, stdoutStderr)
-			startErr = err
-		} else {
-			log.Debug().Msgf("Setup service succesfully: %v for host.", alllowProc.Procname)
-		}
+		startServiceHost(alllowProc.Procname)
 
-		cfgSingleProc, err := yaml.Marshal(alllowProc.Config)
-		if err != nil {
-			log.Error().Err(err).Msgf("Serialize configuration %v", err)
-			startErr = err
-			continue
-		}
-
-		allProc, err := util.ProcessesByName(alllowProc.Procname)
-		if err != nil {
-			log.Error().Err(err).Msgf("Get Process name failed for %v", alllowProc.Procname)
-			startErr = err
-			continue
-		}
-
-		for _, process := range allProc {
-			err := rc.startAttachStage(process, cfgSingleProc)
-			if errors.Is(err, errAlreadyScope) {
-				log.Debug().Msgf("Process PID %v is already scoped", process.Pid)
-				continue
-			}
-			if err != nil {
-				log.Error().Err(err).Msgf("Attach failed for PID %v", process.Pid)
-				startErr = err
-			} else {
-				log.Debug().Msgf("Attach succeded for PID %v", process.Pid)
-			}
-		}
+		startAttach(alllowProc)
 	}
 
 	// Deny list actions

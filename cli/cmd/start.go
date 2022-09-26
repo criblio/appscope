b65fcb3f4be5b2033017e025b58eaca1ee76046a
Sean Valeo <seanvaleo@outlook.com>
2022-09-26 14:49:22 -0400

    update cli/start description

--- cli/cmd/start.go
+++ cli/cmd/start.go
@@ -4,22 +4,25 @@ import (
 	"github.com/spf13/cobra"
 )
 
+/* Args Matrix (X disallows)
+ */
+
 // startCmd represents the start command
 var startCmd = &cobra.Command{
 	Use:   "start",
-	Short: "Perform a scope start operation",
-	Long: `Perform a scope start operation based on the filter input.
+	Short: "Start scoping a filtered selection of processes and services",
+	Long: `Start scoping a filtered selection of processes and services on the host and in all relevant containers.
 
-Following actions will be performed:
-- extraction of the libscope.so to /tmp/libscope.so on the host and on the containers
-- extraction of the filter input to /tmp/scope_filter.yml on the host and on the containers
-- setup etc/profile script to use LD_PRELOAD=/tmp/libscope.so on the host and on the containers
-- setup the services which meet the allow list conditions on the host and on the containers
-- attach to the processes which meet the allow list conditions on the host and on the containers`,
-	Example: `
-	scope start < example_filter.yml
-	cat example_filter.json | scope start
-	`,
+The following actions will be performed on the host and in all relevant containers:
+- Extraction of libscope.so to /tmp/libscope.so 
+- Extraction of the filter input to /tmp/scope_filter.yml
+- Attach to all existing "allowed" processes defined in the filter file
+
+If you choose to persist after reboot, the following will also be performed on the host and in all relevant containers:
+- Install etc/profile script to preload /tmp/libscope.so
+- Modify the relevant service configurations to preload /tmp/libscope.so`,
+	Example: `  scope start < example_filter.yml
+  cat example_filter.json | scope start`,
 	Args: cobra.NoArgs,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return rc.Start()

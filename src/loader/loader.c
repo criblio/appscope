a0c18c384b861ced0519e29d5dde652225a246c8
Sean Valeo <seanvaleo@outlook.com>
2022-12-14 14:30:56 -0500

    add loader dir

---
+++ src/loader/loader.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "loader.h"
+
+int
+loader(int argc, char **argv) {
+    printf("in the loader code\n");
+    return 0;
+}
---
+++ src/loader/loader.h
@@ -0,0 +1,7 @@
+#ifndef __LOADER_H__
+#define __LOADER_H__
+
+int loader(int, char **);
+
+
+#endif // __LOADER_H__

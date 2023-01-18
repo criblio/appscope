450782323f81ed8b8e6d1e3dc2ef6379f3ed493b
michalbiesek <mbiesek@cribl.io>
2023-01-18 23:37:23 +0100

    [contrib] Extend `extract_symbols.sh` libraries with `coredumper` reference

--- extract_symbols.sh
+++ extract_symbols.sh
@@ -39,6 +39,7 @@ declare -a conrib_libs=(
 "./contrib/build/pcre2/libpcre2-8.a"
 "./contrib/build/ls-hpack/libls-hpack.a"
 "./contrib/build/libunwind/src/.libs/libunwind.a"
+"./contrib/build/coredumper/.libs/libcoredumper.a"
 "./contrib/build/musl/lib/libc_orig.a" ## must be last
 )
 

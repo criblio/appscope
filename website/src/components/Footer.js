6608e20aab86a9a06c816d2ecef30d9b95c837c0
Abe Raher <abe@cribl.io>
2023-01-19 12:07:44 -0800

    update footer

--- website/src/components/Footer.js
+++ website/src/components/Footer.js
@@ -16,7 +16,7 @@ export default function Footer() {
             </a>
           </Col>
           <Col xs={12} md={6} className="text-right footer-right">
-            <p>Cribl, &copy; 2021</p>
+            <p>&copy;2017 – 2023 Cribl, Inc. | <a href="https://cribl.io/legal" target="_blank" rel="noopener">Legal</a></p>
             <a href="https://www.facebook.com/Cribl-258234158133458/">
               <FontAwesomeIcon icon={["fab", "facebook"]} />
             </a>

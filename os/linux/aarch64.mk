e8cc4c3520a8f6d0715433d1dc245335159edaca
michalbiesek <mbiesek@cribl.io>
2023-01-24 10:45:00 +0100

    [Building process] Build `coredumper`
    - extend Makefiles to support building `coredumper`

--- os/linux/aarch64.mk
+++ os/linux/aarch64.mk
@@ -7,7 +7,7 @@ LD_FLAGS=$(MUSL_AR) $(UNWIND_AR) $(PCRE2_AR) $(LS_HPACK_AR) $(YAML_AR) $(JSON_AR
 INCLUDES=-I./contrib/libyaml/include -I./contrib/cJSON -I./os/$(OS) -I./contrib/pcre2/src -I./contrib/build/pcre2 -I./contrib/funchook/capstone_src/include/ -I./contrib/jni -I./contrib/jni/linux/ -I./contrib/openssl/include -I./contrib/build/openssl/include -I./contrib/build/libunwind/include  -I./contrib/libunwind/include/
 
 $(LIBSCOPE): src/wrap.c src/state.c src/httpstate.c src/metriccapture.c src/report.c src/httpagg.c src/plattime.c src/fn.c os/$(OS)/os.c src/cfgutils.c src/cfg.c src/transport.c src/backoff.c src/log.c src/mtc.c src/circbuf.c src/linklist.c src/evtformat.c src/ctl.c src/mtcformat.c src/com.c src/scopestdlib.c src/dbg.c src/search.c src/wrap_go.c src/sysexec.c src/gocontext_arm.S src/scopeelf.c src/utils.c src/strset.c src/javabci.c src/javaagent.c src/ipc.c src/ipc_resp.c src/signalhandler.c
-	@$(MAKE) -C contrib funchook pcre2 openssl ls-hpack musl libyaml libunwind cJSON
+	@$(MAKE) -C contrib funchook pcre2 openssl ls-hpack musl libyaml libunwind cJSON coredumper
 	@echo "$${CI:+::group::}Building $@"
 	$(CC) $(LIBRARY_CFLAGS) $(ARCH_CFLAGS) \
 		-shared -fvisibility=hidden -fno-stack-protector \
--- os/linux/x86_64.mk
+++ os/linux/x86_64.mk
@@ -12,7 +12,7 @@ INCLUDES=-I./contrib/libyaml/include -I./contrib/cJSON -I./os/$(OS) -I./contrib/
 #	rm -f ./lib/$(OS)/libscope.so
 
 $(LIBSCOPE): src/wrap.c src/state.c src/httpstate.c src/metriccapture.c src/report.c src/httpagg.c src/plattime.c src/fn.c os/$(OS)/os.c src/cfgutils.c src/cfg.c src/transport.c src/backoff.c src/log.c src/mtc.c src/circbuf.c src/linklist.c src/evtformat.c src/ctl.c src/mtcformat.c src/com.c src/scopestdlib.c src/dbg.c src/search.c src/sysexec.c src/gocontext.S src/scopeelf.c src/wrap_go.c src/utils.c src/strset.c src/javabci.c src/javaagent.c src/ipc.c src/ipc_resp.c src/signalhandler.c
-	@$(MAKE) -C contrib funchook pcre2 openssl ls-hpack musl libyaml cJSON libunwind
+	@$(MAKE) -C contrib funchook pcre2 openssl ls-hpack musl libyaml cJSON libunwind coredumper
 	@echo "$${CI:+::group::}Building $@"
 	$(CC) $(LIBRARY_CFLAGS) $(ARCH_CFLAGS) \
 		-shared -fvisibility=hidden -fno-stack-protector \

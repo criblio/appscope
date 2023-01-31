e0dbb91d1818c0b16446479d7a20c487c93e7f2e
Sean Valeo <seanvaleo@outlook.com>
2023-01-31 14:30:04 -0500

    call sigdel from daemon cmd

--- cli/cmd/daemon.go
+++ cli/cmd/daemon.go
@@ -1,9 +1,7 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/criblio/scope/bpf/sigdel"
-	"github.com/criblio/scope/internal"
 	"github.com/spf13/cobra"
 )
 
@@ -20,32 +18,11 @@ var daemonCmd = &cobra.Command{
 scope daemon`,
 	Args: cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-		summary := internal.GetGitSummary()
-		date := internal.GetBuildDate()
-		tag := internal.GetVersion()
-		if daemonSummary {
-			fmt.Printf("%s\n", summary)
-			return
-		}
-		if daemonDate {
-			fmt.Printf("%s\n", date)
-			return
-		}
-		if daemonTag {
-			fmt.Printf("%s\n", tag)
-			return
-		}
-		fmt.Printf("Version: %s\n", summary)
-		fmt.Printf("Build Date: %s\n", date)
+
+		sigdel.Sigdel()
 	},
-
-
-	Sigdel()
 }
 
 func init() {
 	RootCmd.AddCommand(daemonCmd)
-	daemonCmd.Flags().BoolVar(&daemonSummary, "summary", false, "Output just the summary")
-	daemonCmd.Flags().BoolVar(&daemonDate, "date", false, "Output just the date")
-	daemonCmd.Flags().BoolVar(&daemonTag, "tag", false, "Output just the tag")
 }

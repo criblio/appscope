54fe2b83b6789b110c0e6a2d0ce61b0a9986769a
Sean Valeo <seanvaleo@outlook.com>
2023-02-10 12:49:00 -0500

    refactor daemon loop

--- cli/cmd/daemon.go
+++ cli/cmd/daemon.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/criblio/scope/daemon"
 	"github.com/criblio/scope/util"
 	"github.com/rs/zerolog/log"
@@ -26,17 +28,19 @@ var daemonCmd = &cobra.Command{
 
 		d := daemon.New(filedest)
 
-		/*
-			sigEventChan := make(chan sigdel.SigEvent)
-			go sigdel.Sigdel(sigEventChan)
+		sigEventChan := make(chan int, 2)
+		sigEventChan <- 150811
+		// sigEventChan := make(chan sigdel.SigEvent)
+		// go sigdel.Sigdel(sigEventChan)
 
-			for {
-				select {
+		for {
+			select {
+			/*
 				case sigEvent := <-sigEventChan:
 					// Signal received
 					log.Info().Msgf("Signal CPU: %02d signal %d errno %d handler 0x%x pid: %d uid: %d gid: %d app %s\n",
-						sigEvent.CPU, sigEvent.Sig, sigEvent.Errno, sigEvent.Handler,
-						sigEvent.Pid, sigEvent.Uid, sigEvent.Gid, sigEvent.Comm)
+					sigEvent.CPU, sigEvent.Sig, sigEvent.Errno, sigEvent.Handler,
+					sigEvent.Pid, sigEvent.Uid, sigEvent.Gid, sigEvent.Comm)
 
 					// Generate/retrieve crash files
 					if err := crash.GenFiles(sigEvent.Pid, sigEvent.Sig, sigEvent.Errno); err != nil {
@@ -46,40 +50,38 @@ var daemonCmd = &cobra.Command{
 
 					// If a network destination is specified, send crash files
 					// See below example usage
+			*/
+			case pid := <-sigEventChan:
+				if filedest != "" {
+					if err := d.Connect(); err != nil {
+						log.Error().Err(err)
+						util.ErrAndExit("error starting daemon")
+					}
+
+					files := []string{
+						fmt.Sprintf("/tmp/appscope/%d/snapshot", pid),
+						fmt.Sprintf("/tmp/appscope/%d/backtrace", pid),
+						fmt.Sprintf("/tmp/appscope/%d/info", pid),
+						fmt.Sprintf("/tmp/appscope/%d/cfg", pid),
+					}
+					if sendcore {
+						files = append(files, fmt.Sprintf("/tmp/appscope/%d/core", pid))
+					}
+
+					if err := d.SendFiles(files); err != nil {
+						log.Error().Err(err)
+						util.ErrAndExit("error sending files to %s", filedest)
+					}
+
+					d.Disconnect()
 				}
 			}
-		*/
-
-		// Example usage
-		if filedest != "" {
-			if err := d.Connect(); err != nil {
-				log.Error().Err(err)
-				util.ErrAndExit("error starting daemon")
-			}
-
-			files := []string{
-				"/tmp/appscope/150811/snapshot",
-				"/tmp/appscope/150811/backtrace",
-				"/tmp/appscope/150811/info",
-				"/tmp/appscope/150811/cfg",
-			}
-			if sendcore {
-				files = append(files, "/tmp/appscope/150811/core")
-			}
-
-			if err := d.SendFiles(files); err != nil {
-				log.Error().Err(err)
-				util.ErrAndExit("error sending files to %s", filedest)
-			}
-
-			d.Disconnect()
 		}
-
 	},
 }
 
 func init() {
-	daemonCmd.Flags().StringP("filedest", "f", "", "Set destination for files (host:port defaults to tls://)")
+	daemonCmd.Flags().StringP("filedest", "f", "", "Set destination for files (host:port defaults to tcp://)")
 	daemonCmd.Flags().BoolP("sendcore", "s", false, "Include core file when sending files to network destination")
 	RootCmd.AddCommand(daemonCmd)
 }

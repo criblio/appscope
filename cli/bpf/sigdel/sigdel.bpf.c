7c216c45b05448a2e2e3647cf4716c28a4dfb498
Donn <donn@cribl.io>
2023-02-10 16:07:32 -0600

    eBPF kernel code returns the correct pid; host or container.

--- cli/bpf/sigdel/sigdel.bpf.c
+++ cli/bpf/sigdel/sigdel.bpf.c
@@ -73,37 +73,33 @@ int sig_deliver(struct sigdel_args_t *args)
     }
 #endif
 	struct sigdel_data_t sigdel_data = {};
-	u64 pid_tgid, uid_gid;;
+	u64 pid_tgid, uid_gid;
+    struct task_struct *task;
+    struct pid *pid;
+    unsigned int level, nr;
+    struct upid upid;
+    struct ns_common ns;
 
-    //bpf_printk("sigdel started: %d\n", args->sig);
+    task = bpf_get_current_task_btf();
+    if (!task) {
+        bpf_printk("ERROR:%d:sigdel:get current task\n", __LINE__);
+        return 0;
+    }
 
-	pid_tgid = bpf_get_current_pid_tgid();
-	sigdel_data.pid = LAST_32_BITS(pid_tgid);
+    // TODO: add error checking
+    bpf_probe_read_kernel(&pid, sizeof(pid), &task->thread_pid);
+    bpf_probe_read_kernel(&level, sizeof(level), &pid->level);
+    bpf_probe_read_kernel(&upid, sizeof(upid), &pid->numbers[level]);
+    bpf_probe_read_kernel(&ns, sizeof(ns), &upid.ns->ns);
+    bpf_probe_read_kernel(&nr, sizeof(nr), &upid.nr);
+
+    bpf_printk("sigdel: %s: ino %u nr %u\n", task->comm, ns.inum, nr);
+
+    sigdel_data.pid = nr;
 
     uid_gid = bpf_get_current_uid_gid();
     sigdel_data.uid = LAST_32_BITS(uid_gid);
     sigdel_data.gid = FIRST_32_BITS(uid_gid);
-
-
-    if (bpf_probe_read(&sigdel_data.sig, sizeof(sigdel_data.sig), &args->sig) != 0) {
-        bpf_printk("ERROR:sigdel:bpf_probe_read:sig number\n");
-        sigdel_data.sig = -1;
-    }
-
-    if (bpf_probe_read(&sigdel_data.errno, sizeof(sigdel_data.errno), &args->errno) != 0) {
-        bpf_printk("ERROR:sigdel:bpf_probe_read:errno\n");
-        sigdel_data.errno = -1;
-    }
-
-    if (bpf_probe_read(&sigdel_data.code, sizeof(sigdel_data.code), &args->code) != 0) {
-        bpf_printk("ERROR:sigdel:bpf_probe_read:code\n");
-        sigdel_data.code = -1;
-    }
-
-    if (bpf_probe_read(&sigdel_data.sa_handler, sizeof(sigdel_data.sa_handler), &args->sa_handler) != 0) {
-        bpf_printk("ERROR:sigdel:bpf_probe_read:sa_handler\n");
-        sigdel_data.sa_handler = (unsigned long)-1;
-    }
 
 	if (bpf_get_current_comm(sigdel_data.comm, sizeof(sigdel_data.comm)) != 0) {
         bpf_printk("ERROR:sigdel:bpf_get_current_comm\n");
@@ -113,12 +109,15 @@ int sig_deliver(struct sigdel_args_t *args)
         sigdel_data.comm[3] = '\0';
     }
 
+    sigdel_data.sig = args->sig;
+    sigdel_data.errno = args->errno;
+    sigdel_data.code = args->code;
+    sigdel_data.sa_handler = args->sa_handler;
+
 	if (bpf_perf_event_output(args, &events, BPF_F_CURRENT_CPU,
                               &sigdel_data, sizeof(sigdel_data)) != 0) {
         bpf_printk("ERROR:sigdel:bpf_perf_event_output\n");
     }
-
-	bpf_printk("sigdel happened: %s\n", sigdel_data.comm);
 
 	return 0;
 }

f0f7ea53e1842df27883990620cd2126a2810b84
michalbiesek <mbiesek@cribl.io>
2023-03-29 19:03:27 +0200

    Add test for checking forbidden symbols

---
+++ test/unit/verboten_sym.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+SCOPE_LIB=./lib/linux/$(uname -m)/libscope.so
+
+# List of forbidden symbols
+declare -a verboten_syms=(
+"secure_getenv"
+)
+
+declare -i EXIT_STATUS=0
+echo "================================="
+echo "      Verboten  Symbol Test      "
+echo "================================="
+
+for sym in "${verboten_syms[@]}"
+do
+    if nm "$SCOPE_LIB" | grep -w "$sym";
+    then
+        EXIT_STATUS=1
+        echo "Test failed symbol $sym should not be present in $SCOPE_LIB"
+    fi
+done
+
+if (( "$EXIT_STATUS" == 0 )); then
+    echo "Success"
+fi
+
+exit ${EXIT_STATUS}

44b9c4dd68e8d11f98f92c407e6ca9afa1c50ce7
iapaddler <donn@cribl.io>
2023-04-03 18:59:30 +0000

    Description, handle console msgs, test with multiple pid dirs.

--- cli/promserver/promserver.go
+++ cli/promserver/promserver.go
@@ -7,12 +7,45 @@ import (
 	"os"
 )
 
+/*
+ * This represents the handler that runs in response to an
+ * HTTP GET /metrics request, from a Prometheus scraper.
+ *
+ * The primary use case is that of a k8s cluster.
+ * It is expected that this executes from a container with a
+ * hostfs read-only mount point to the host FS.
+ *
+ * The handler searches /hostfs/proc/<pid> for a root/scope/metrics
+ * entry. Where /scope at a k8s pod level is defined by a
+ * scope k8s command. It then walks subdirs, intended to include
+ * process specific metrics. An HTTP response is sent with all
+ * available metrics. The intent is that all application metrics
+ * emitted from libscope for all scoped apps will be accesible.
+ * 
+ * The paths used are currently hard coded. At least the start point
+ * for a search should probably be passed as command line options.
+ * The specific subdirs and file name must be compatible with
+ * libscope behavior, yet to be defined at this point.
+ * example: /hostfs/proc/4693/root/scope/metrics/1/metrics.json
+ */
+
 const pelement1 = "/hostfs"
 const pelement2 = "proc/"
 const pelement3 = "root/"
 const pelement4 = "scope/"
 const pelement5 = "metrics/"
 const pelement6 = "metrics.json"
+
+var msgs = 1
+
+func pmsg(msg ...interface{}) {
+     if msgs != 0 {
+        for _, pvar:= range msg {
+            fmt.Print(pvar, " ")
+        }
+        fmt.Println()
+     }
+}
 
 func faccess(path string) (bool) {
     if _, err := os.Stat(path); err != nil {
@@ -24,7 +57,7 @@ func faccess(path string) (bool) {
 
 func Handler(rwrite http.ResponseWriter, req *http.Request) {
 	path := filepath.Join(pelement1, pelement2)
-	fmt.Println("checking dir: ", path)
+    pmsg("checking dir: ", path)
 	dir, err := os.ReadDir(path)
 	if err != nil {
         fmt.Println(err)
@@ -56,17 +89,16 @@ func Handler(rwrite http.ResponseWriter, req *http.Request) {
 
                mdname, _ := metricd.Readdirnames(0)
                for _, ename := range mdname {
-                     // /hostfs/proc/dname/root/scope/metrics/mdname/metrics.json
+                     // /hostfs/proc/dname/root/scope/metrics/ename/metrics.json
                      path := filepath.Join(pelement1, pelement2, dname,	pelement3,
                           pelement4, pelement5, ename, pelement6)
 
-                     fmt.Println(err, path, mdname)
                      metricf, err := os.Open(path)
                      if err != nil {
                         continue
 			         }
 
-			         fmt.Println("found pid: ", dname, "file: ", path)
+                     pmsg("found pid: ", dname, "file: ", path)
 
 			         metrici, err := metricf.Stat()
                      if err != nil {
@@ -79,7 +111,7 @@ func Handler(rwrite http.ResponseWriter, req *http.Request) {
 				        continue
 			         }
 
-			         fmt.Println("read ", metricd, " bytes")
+                     pmsg("read ", metricd, " bytes")
 
 			         if _, err := tempf.Write(metricbuf); err != nil {
 				        continue
@@ -107,7 +139,7 @@ func Handler(rwrite http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	fmt.Println("read ", tempd, " bytes", "temp size: ", tempi.Size())
+    pmsg("read ", tempd, " bytes", "temp size: ", tempi.Size())
 
     rwrite.Header().Set("Content-Type", "text/plain")
 	rwrite.Write(tempbuf)

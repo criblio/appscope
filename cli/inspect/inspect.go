30b4c1cbfd9a5951337b0eabd911d16cc4b0bd8a
michalbiesek <mbiesek@cribl.io>
2023-04-17 16:13:04 +0200

    (#1415) Extend IPC with details about process
    - provide information about pid, UUID, id and machine_id

--- cli/inspect/inspect.go
+++ cli/inspect/inspect.go
@@ -9,14 +9,28 @@ import (
 
 var errInspectCfg = errors.New("error inspect cfg")
 
+// procDetails describes details of process
+type procDetails struct {
+	// Pid
+	Pid int `mapstructure:"pid" json:"pid" yaml:"pid"`
+	// UUID
+	Uuid string `mapstructure:"uuid" json:"uuid" yaml:"uuid"`
+	// Id
+	Id string `mapstructure:"id" json:"id" yaml:"id"`
+	// Machine id
+	MachineId string `mapstructure:"machine_id" json:"machine_id" yaml:"machine_id"`
+}
+
 type inspectOutput struct {
-	Cfg  ipc.ScopeGetCfgResponseCfg `mapstructure:"cfg" json:"cfg" yaml:"cfg"`
-	Desc ipc.ScopeInterfaceDesc     `mapstructure:"interfaces" json:"interfaces" yaml:"interfaces"`
+	Cfg     ipc.ScopeGetCfgResponseCfg `mapstructure:"cfg" json:"cfg" yaml:"cfg"`
+	Desc    ipc.ScopeInterfaceDesc     `mapstructure:"interfaces" json:"interfaces" yaml:"interfaces"`
+	Process procDetails                `mapstructure:"process" json:"process" yaml:"process"`
 }
 
 // InspectProcess returns the configuration and status of scoped process
 func InspectProcess(pidCtx ipc.IpcPidCtx) (string, error) {
 
+	// Get configuration
 	cmdGetCfg := ipc.CmdGetScopeCfg{}
 	resp, err := cmdGetCfg.Request(pidCtx)
 	if err != nil {
@@ -31,6 +45,7 @@ func InspectProcess(pidCtx ipc.IpcPidCtx) (string, error) {
 		return "", errInspectCfg
 	}
 
+	// Get transport status
 	cmdGetTransportStatus := ipc.CmdGetTransportStatus{}
 	respTr, err := cmdGetTransportStatus.Request(pidCtx)
 	if err != nil {
@@ -46,9 +61,31 @@ func InspectProcess(pidCtx ipc.IpcPidCtx) (string, error) {
 		return "", errInspectCfg
 	}
 
+	// Get process details
+	cmdGetProcDetails := ipc.CmdGetProcessDetails{}
+	respPd, err := cmdGetProcDetails.Request(pidCtx)
+	if err != nil {
+		return "", err
+	}
+
+	err = cmdGetProcDetails.UnmarshalResp(respPd.ResponseScopeMsgData)
+	if err != nil {
+		return "", err
+	}
+
+	if respPd.MetaMsgStatus != ipc.ResponseOK || *cmdGetProcDetails.Response.Status != ipc.ResponseOK {
+		return "", errInspectCfg
+	}
+
 	summary := inspectOutput{
 		Cfg:  cmdGetCfg.Response.Cfg,
 		Desc: cmdGetTransportStatus.Response.Interfaces,
+		Process: procDetails{
+			Pid:       cmdGetProcDetails.Response.Pid,
+			Uuid:      cmdGetProcDetails.Response.Uuid,
+			Id:        cmdGetProcDetails.Response.Id,
+			MachineId: cmdGetProcDetails.Response.MachineId,
+		},
 	}
 
 	sumPrint, err := json.MarshalIndent(summary, "", "   ")

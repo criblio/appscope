c6ab6be9af74cf4b92767a4c034c27b1c3805be5
michalbiesek <mbiesek@cribl.io>
2023-06-05 22:08:56 +0200

    [Remove this commit] ConfigFromStdin debug

--- cli/run/attach.go
+++ cli/run/attach.go
@@ -67,6 +67,8 @@ func (rc *Config) Attach(args []string) (int, error) {
 
 	env := os.Environ()
 
+	log.Error().Msg("Attach append SCOPE_FILTER=false")
+
 	// Disable detection of a scope filter file with this command
 	env = append(env, "SCOPE_FILTER=false")
 
@@ -75,6 +77,7 @@ func (rc *Config) Attach(args []string) (int, error) {
 		env = append(env, "SCOPE_CRIBL_NO_BREAKER=true")
 	}
 
+	log.Error().Msg("Attach before ConfigFromStdin")
 	// Read config from stdin if it exists
 	if err := rc.ConfigFromStdin(); err != nil {
 		return pid, err
--- cli/run/scopeconfig.go
+++ cli/run/scopeconfig.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 
 	"github.com/criblio/scope/libscope"
+	"github.com/rs/zerolog/log"
 	"gopkg.in/yaml.v2"
 )
 
@@ -130,24 +131,30 @@ func (c *Config) SetDefault() error {
 // ConfigFromStdin loads a configuration from yml passed to stdin
 func (c *Config) ConfigFromStdin() error {
 	var cfgData []byte
+	var cfgErr error
 
 	stdinFs, err := os.Stdin.Stat()
 	if err != nil {
+		fmt.Fprintf(os.Stderr, "ConfigFromStdin os.Stdin.Stat() %v", err)
 		return nil
 	}
 
 	// Avoid waiting for input from terminal when no data was provided
 	if stdinFs.Mode()&os.ModeCharDevice != 0 && stdinFs.Size() == 0 {
+		fmt.Fprintln(os.Stderr, "ConfigFromStdin stdinFs.Mode()&os.ModeCharDevice")
 		return nil
 	}
 
-	cfgData, _ = io.ReadAll(os.Stdin)
+	cfgData, cfgErr = io.ReadAll(os.Stdin)
 
+	fmt.Fprintf(os.Stderr, "ConfigFromStdin io.Readall data %v error %v", cfgData, cfgErr)
 	c.sc = &libscope.ScopeConfig{}
 
 	if err := yaml.Unmarshal(cfgData, &c.sc); err != nil {
+		log.Error().Msgf("ConfigFromStdin Unmarshal %v", err)
 		return err
 	}
+	fmt.Fprintln(os.Stderr, "ConfigFromStdin finish with nil")
 
 	return nil
 }

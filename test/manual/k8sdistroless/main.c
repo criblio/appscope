090737a968914cc82448ec0685019b301defa964
michalbiesek <mbiesek@cribl.io>
2023-06-12 11:32:35 +0200

    Extend manual test with distroless k8s deployment

---
+++ test/manual/k8sdistroless/main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE] = {0};
+    char *response = "Hello World from the server!";
+
+    // Create socket
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set socket options
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Bind the socket to a specific address and port
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for incoming connections
+    if (listen(server_fd, 3) < 0) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d\n", PORT);
+
+    while (1) {
+        // Accept a new connection
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+            perror("accept failed");
+            exit(EXIT_FAILURE);
+        }
+
+        // Read data from the client
+        if (read(new_socket, buffer, BUFFER_SIZE) < 0) {
+            perror("read failed");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Received request: %s\n", buffer);
+
+        // Send a response to the client
+        send(new_socket, response, strlen(response), 0);
+        printf("Response sent: %s\n", response);
+
+        // Clear the buffer and close the socket
+        memset(buffer, 0, sizeof(buffer));
+        close(new_socket);
+    }
+
+    return 0;
+}

7c5cfca87bcdcd2f3885547c61618b1e1c1aa0a3
Donn <donn@cribl.io>
2023-07-25 18:47:51 +0000

    Added support to locate a session by id.

--- test/manual/reports/files.sh
+++ test/manual/reports/files.sh
@@ -3,6 +3,7 @@
 SCOPE_PATH="."
 RESULT="./file_results.txt"
 cmd=""
+id=""
 dir=0
 
 while test $# -gt 0
@@ -18,6 +19,9 @@ do
                ;;
         --raw) raw=$2
                echo "using the events file at $2."
+               ;;
+        --id) id="--id $2"
+               echo "using the events file defined by id $2."
                ;;
         --*) echo "bad option $1"
                ;;
@@ -36,8 +40,7 @@ if [[ $raw != "" ]]; then
         grep fs.open $raw | jq '.body.data | "\(.file)"' | sort | uniq > $RESULT
     fi
 elif [ $dir -ne 0  ]; then
-    $SCOPE_PATH/scope events -aj | grep '"open"' | jq -r '.data.file' | xargs dirname | sort | uniq  > $RESULT    
+    $SCOPE_PATH/scope events -aj $id | grep '"fs.open"' | jq -r '.data.file' | xargs dirname | sort | uniq  > $RESULT
 else
-    $SCOPE_PATH/scope events -aj | grep '"open"' | jq -r '.data.file' | sort | uniq > $RESULT
+    $SCOPE_PATH/scope events -aj $id | grep '"fs.open"' | jq -r '.data.file' | sort | uniq > $RESULT
 fi
-
